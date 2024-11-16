6b177279421e564efd47e53444e7e078823b45fc
𝕂𝕪𝕝𝕚𝕒𝕟 <kylian.jarry@scaramouch.com>
2024-11-16 22:13:39 +0100

    ajout animation sur `login/pronote.ts`

--- src/router/screens/login/pronote.ts
+++ src/router/screens/login/pronote.ts
@@ -11,54 +11,63 @@ import PronoteWebview from "@/views/login/pronote/PronoteWebview";
 import PronoteV6Import from "@/views/login/pronote/PronoteV6Import";
 import Pronote2FA_Auth from "@/views/login/pronote/Pronote2FA_Auth";
 
-
 export default [
   createScreen("PronoteAuthenticationSelector", PronoteAuthenticationSelector, {
     headerTitle: "",
     headerTransparent: true,
-    headerBackVisible: true
+    headerBackVisible: true,
+    animation: "slide_from_right",
   }),
   createScreen("PronoteGeolocation", PronoteGeolocation, {
     headerTitle: "",
     headerTransparent: true,
-    headerBackVisible: true
+    headerBackVisible: true,
+    animation: "slide_from_right",
   }),
   createScreen("PronoteInstanceSelector", PronoteInstanceSelector, {
     headerTitle: "",
     headerTransparent: true,
-    headerBackVisible: true
+    headerBackVisible: true,
+    animation: "slide_from_right",
   }),
   createScreen("PronoteManualLocation", PronoteManualLocation, {
     headerTitle: "",
     headerTransparent: true,
-    headerBackVisible: true
+    headerBackVisible: true,
+    animation: "slide_from_right",
   }),
   createScreen("PronoteQRCode", PronoteQRCode, {
     headerTitle: "",
     headerTintColor: "white",
     headerTransparent: true,
-    headerBackVisible: true
+    headerBackVisible: true,
+    animation: "slide_from_right",
   }),
   createScreen("PronoteCredentials", PronoteCredentials, {
     headerTitle: "Connexion à PRONOTE",
-    headerBackVisible: true
+    headerBackVisible: true,
+    animation: "slide_from_right",
   }),
   createScreen("PronoteManualURL", PronoteManualURL, {
     headerTitle: "",
     headerTransparent: true,
-    headerBackVisible: true
+    headerBackVisible: true,
+    animation: "slide_from_right",
   }),
   createScreen("PronoteWebview", PronoteWebview, {
     headerTitle: "",
     headerTransparent: true,
-    headerBackVisible: true
+    headerBackVisible: true,
+    animation: "slide_from_right",
   }),
   createScreen("PronoteV6Import", PronoteV6Import, {
     headerTitle: "Importer d'une ancienne version",
     presentation: "modal",
+    animation: "slide_from_right",
   }),
   createScreen("Pronote2FA_Auth", Pronote2FA_Auth, {
     headerTitle: "Connexion à PRONOTE",
-    headerBackVisible: true
+    headerBackVisible: true,
+    animation: "slide_from_right",
   }),
 ] as const;
