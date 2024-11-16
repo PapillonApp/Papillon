b7bfc45f7b56cab027809e22d83ed831596599f3
𝕂𝕪𝕝𝕚𝕒𝕟 <kylian.jarry@scaramouch.com>
2024-11-16 22:12:06 +0100

    ajout animation sur `login/identityProvider.ts`

--- src/router/screens/login/identityProvider.ts
+++ src/router/screens/login/identityProvider.ts
@@ -5,7 +5,6 @@ import UnivRennes1_Login from "@/views/login/IdentityProvider/providers/UnivRenn
 import UnivLimoges_Login from "@/views/login/IdentityProvider/providers/UnivLimoges";
 import UnivRennes2_Login from "@/views/login/IdentityProvider/providers/UnivRennes2";
 import UnivSorbonneParisNord_login from "@/views/login/IdentityProvider/providers/UnivSorbonneParisNord";
-//import UnivUphf_Login from "@/views/login/IdentityProvider/providers/UnivUphf";
 import { UnivIUTLannion_Login } from "@/views/login/IdentityProvider/providers/UnivIUTLannion";
 import Muli_Login from "@/views/login/IdentityProvider/providers/Multi";
 
@@ -13,35 +12,42 @@ export default [
   createScreen("IdentityProviderSelector", IdentityProviderSelector, {
     headerTitle: "Universités et autres",
     headerBackVisible: true,
+    animation: "slide_from_right",
   }),
 
   createScreen("UnivRennes1_Login", UnivRennes1_Login, {
     headerBackVisible: true,
     headerTitle: "Université de Rennes",
+    animation: "slide_from_right",
   }),
 
   createScreen("UnivRennes2_Login", UnivRennes2_Login, {
     headerBackVisible: true,
     headerTitle: "Université de Rennes 2",
+    animation: "slide_from_right",
   }),
 
   createScreen("UnivIUTLannion_Login", UnivIUTLannion_Login, {
     headerBackVisible: true,
     headerTitle: "IUT de Lannion",
+    animation: "slide_from_right",
   }),
 
   createScreen("UnivLimoges_Login", UnivLimoges_Login, {
     headerBackVisible: true,
     headerTitle: "Université de Limoges",
+    animation: "slide_from_right",
   }),
 
   createScreen("UnivSorbonneParisNord_login", UnivSorbonneParisNord_login, {
     headerBackVisible: true,
     headerTitle: "Université Sorbonne Paris Nord",
+    animation: "slide_from_right",
   }),
 
   createScreen("Multi_Login", Muli_Login, {
     headerBackVisible: true,
     headerTitle: "ESUP Multi",
+    animation: "slide_from_right",
   }),
 ] as const;
