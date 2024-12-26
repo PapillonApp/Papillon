5dc99dea583148f39960248d0abef998d8f232ff
𝕂𝕪𝕝𝕚𝕒𝕟 <kylian.jarry@scaramouch.com>
2024-12-26 21:35:52 +0100

    fix(Home): adjust animated styles for Android platform compatibility

--- src/views/account/Home/Home.tsx
+++ src/views/account/Home/Home.tsx
@@ -39,6 +39,7 @@ import {useIsFocused, useTheme} from "@react-navigation/native";
 import React, { useMemo, useState } from "react";
 import {
   Dimensions,
+  Platform,
   StatusBar,
   View
 } from "react-native";
@@ -90,121 +91,214 @@ const Home: Screen<"HomeScreen"> = ({ navigation }) => {
   });
 
   const widgetAnimatedStyle = useAnimatedStyle(() => {
-    if (modalOpen) {
+    if (Platform.OS === "android") {
+      if (modalOpen) {
+        return {
+          transform: [{ scale: 1 }],
+          opacity: 1,
+        };
+      }
+
+      const translateY = interpolate(
+        scrollOffset.value,
+        [0, 265],
+        [0, 0],
+        Extrapolation.CLAMP
+      );
+      const scale = interpolate(
+        scrollOffset.value,
+        [0, 265],
+        [1, 0.95],
+        Extrapolation.CLAMP
+      );
+      const opacity = interpolate(
+        scrollOffset.value,
+        [0, 200, 265],
+        [1, 1, 0],
+        Extrapolation.CLAMP
+      );
+
       return {
-        transform: [{ scale: 1 }],
-        opacity: 1,
+        paddingTop: insets.top,
+        opacity,
+        transform: [{ translateY }, { scale }],
+      };
+    } else {
+      return {
+        paddingTop: insets.top,
+        opacity: interpolate(
+          scrollOffset.value,
+          [0, 265 + insets.top],
+          [1, 0],
+          Extrapolation.CLAMP
+        ),
+        transform: [
+          { translateY: scrollOffset.value },
+          { scale: interpolate(
+            scrollOffset.value,
+            [0, 265],
+            [1, 0.9],
+            Extrapolation.CLAMP
+          )},
+        ]
       };
     }
-
-    const translateY = interpolate(
-      scrollOffset.value,
-      [0, 265],
-      [0, 0],
-      Extrapolation.CLAMP
-    );
-    const scale = interpolate(
-      scrollOffset.value,
-      [0, 265],
-      [1, 0.95],
-      Extrapolation.CLAMP
-    );
-    const opacity = interpolate(
-      scrollOffset.value,
-      [0, 200, 265],
-      [1, 1, 0],
-      Extrapolation.CLAMP
-    );
-
-    return {
-      paddingTop: insets.top,
-      opacity,
-      transform: [{ translateY }, { scale }],
-    };
   });
 
   const modalAnimatedStyle = useAnimatedStyle(() => {
-    const isScrolling = scrollOffset.value > 0;
+    if (Platform.OS === "android") {
+      const isScrolling = scrollOffset.value > 0;
 
-    const borderRadius = isScrolling
-      ? 0
-      : interpolate(
+      const borderRadius = isScrolling
+        ? 0
+        : interpolate(
+          scrollOffset.value,
+          [0, 100, 265 + insets.top - 1, 265 + insets.top],
+          [12, 12, corners, 0],
+          Extrapolation.CLAMP
+        );
+
+      const scale = isScrolling
+        ? 1
+        : interpolate(
+          scrollOffset.value,
+          [0, 200, 260 + insets.top - 40, 260 + insets.top],
+          [1, 0.95, 0.95, 1],
+          Extrapolation.CLAMP
+        );
+
+      const translateY = interpolate(
         scrollOffset.value,
-        [0, 100, 265 + insets.top - 1, 265 + insets.top],
-        [12, 12, corners, 0],
+        [-1000, 0, 125, 265],
+        [-1000, 0, 105, 0],
         Extrapolation.CLAMP
       );
 
-    const scale = isScrolling
-      ? 1
-      : interpolate(
-        scrollOffset.value,
-        [0, 200, 260 + insets.top - 40, 260 + insets.top],
-        [1, 0.95, 0.95, 1],
-        Extrapolation.CLAMP
-      );
+      return {
+        flex: 1,
+        minHeight: windowHeight - tabbarHeight - 8,
+        backgroundColor: colors.card,
+        overflow: "hidden",
+        borderTopLeftRadius: borderRadius,
+        borderTopRightRadius: borderRadius,
+        transform: [{ scale }, { translateY }],
+        shadowColor: "#000",
+        shadowOpacity: 0.2,
+        shadowRadius: 10,
+      };
+    } else {
+      return {
+        borderTopLeftRadius: interpolate(
+          scrollOffset.value,
+          [0, 100, 265 + insets.top - 1, 265 + insets.top],
+          [12, 12, corners, 0],
+          Extrapolation.CLAMP
+        ),
+        borderTopRightRadius: interpolate(
+          scrollOffset.value,
+          [0, 100, 265 + insets.top - 1, 265 + insets.top],
+          [12, 12, corners, 0],
+          Extrapolation.CLAMP
+        ),
 
-    const translateY = interpolate(
-      scrollOffset.value,
-      [-1000, 0, 125, 265],
-      [-1000, 0, 105, 0],
-      Extrapolation.CLAMP
-    );
+        shadowColor: "#000",
+        shadowOpacity: 0.2,
+        shadowRadius: 10,
 
-    return {
-      flex: 1,
-      minHeight: windowHeight - tabbarHeight - 8,
-      backgroundColor: colors.card,
-      overflow: "hidden",
-      borderTopLeftRadius: borderRadius,
-      borderTopRightRadius: borderRadius,
-      transform: [{ scale }, { translateY }],
-      shadowColor: "#000",
-      shadowOpacity: 0.2,
-      shadowRadius: 10,
-    };
+        flex: 1,
+        minHeight: windowHeight - tabbarHeight - 8,
+        backgroundColor: colors.card,
+        overflow: "hidden",
+        transform: [
+          {scale: interpolate(
+            scrollOffset.value,
+            [0, 200, (260 + insets.top) - 40, 260 + insets.top],
+            [1, 0.95, 0.95, 1],
+            Extrapolation.CLAMP
+          )},
+          {translateY: interpolate(
+            scrollOffset.value,
+            [-1000, 0, 125, 265 ],
+            [-1000, 0, 105, 0],
+            Extrapolation.CLAMP
+          )}
+        ],
+      };
+    }
   });
 
   const modalIndicatorAnimatedStyle = useAnimatedStyle(() => {
-    const isScrolling = scrollOffset.value > 0;
+    if (Platform.OS === "android") {
+      const isScrolling = scrollOffset.value > 0;
 
-    return {
-      position: "absolute",
-      top: 10,
-      left: "50%",
-      transform: [
-        {
-          translateX: isScrolling
-            ? -2
-            : interpolate(
-              scrollOffset.value,
-              [125, 200],
-              [-25, -2],
-              Extrapolation.CLAMP
-            ),
-        },
-      ],
-      width: isScrolling
-        ? 4
-        : interpolate(
+      return {
+        position: "absolute",
+        top: 10,
+        left: "50%",
+        transform: [
+          {
+            translateX: isScrolling
+              ? -2
+              : interpolate(
+                scrollOffset.value,
+                [125, 200],
+                [-25, -2],
+                Extrapolation.CLAMP
+              ),
+          },
+        ],
+        width: isScrolling
+          ? 4
+          : interpolate(
+            scrollOffset.value,
+            [125, 200],
+            [50, 4],
+            Extrapolation.CLAMP
+          ),
+        height: 4,
+        backgroundColor: colors.text + "20",
+        zIndex: 100,
+        borderRadius: 5,
+        opacity: isScrolling
+          ? 0
+          : interpolate(
+            scrollOffset.value,
+            [125, 180, 200],
+            [1, 0.5, 0],
+            Extrapolation.CLAMP
+          ),
+      };
+    } else {
+      return {
+        position: "absolute",
+        top: 10,
+        left: "50%",
+        transform: [
+          {translateX: interpolate(
+            scrollOffset.value,
+            [125, 200],
+            [-25, -2],
+            Extrapolation.CLAMP
+          )}
+        ],
+        width: interpolate(
           scrollOffset.value,
           [125, 200],
           [50, 4],
           Extrapolation.CLAMP
         ),
-      height: 4,
-      backgroundColor: colors.text + "20",
-      zIndex: 100,
-      borderRadius: 5,
-      opacity: isScrolling
-        ? 0
-        : interpolate(
+        height: 4,
+        backgroundColor: colors.text + "20",
+        zIndex: 100,
+        borderRadius: 5,
+        opacity: interpolate(
           scrollOffset.value,
           [125, 180, 200],
           [1, 0.5, 0],
           Extrapolation.CLAMP
         ),
-    };
+      };
+    }
   });
 
   const modalContentAnimatedStyle = useAnimatedStyle(() => ({
