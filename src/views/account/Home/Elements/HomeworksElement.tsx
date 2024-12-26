fc5ae103f7479e9ba7a309e32bd5b11c08f306f9
𝕂𝕪𝕝𝕚𝕒𝕟 <kylian.jarry@scaramouch.com>
2024-12-26 21:51:42 +0100

    adaptation de la logique quand une des 2 variables des devoirs est undefined

--- src/views/account/Home/Elements/HomeworksElement.tsx
+++ src/views/account/Home/Elements/HomeworksElement.tsx
@@ -65,16 +65,12 @@ const HomeworksElement: React.FC<HomeworksElementProps> = ({ navigation, onImpor
 
   const hwSemaineActuelle = homeworks[dateToEpochWeekNumber(actualDay)]?.filter(
     (hw) => hw.due / 1000 >= startTime && hw.due / 1000 <= endTime
-  );
+  ) ?? [];
   const hwSemaineProchaine = homeworks[dateToEpochWeekNumber(actualDay) + 1]?.filter(
     (hw) => hw.due / 1000 >= startTime && hw.due / 1000 <= endTime
-  );
+  ) ?? [];
 
-
-  if (
-    (!hwSemaineActuelle && !hwSemaineProchaine) ||
-    (hwSemaineActuelle.length === 0 && hwSemaineProchaine.length === 0)
-  ) {
+  if (hwSemaineActuelle.length === 0 && hwSemaineProchaine.length === 0) {
     return (
       <NativeList
         animated
