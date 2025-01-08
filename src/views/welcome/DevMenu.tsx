e435e831456b14f416f009b2a35c5f990059cabb
𝕂𝕪𝕝𝕚𝕒𝕟 <kylian.jarry@scaramouch.com>
2025-01-08 19:50:06 +0100

    fix(ts) errors

--- src/views/welcome/DevMenu.tsx
+++ src/views/welcome/DevMenu.tsx
@@ -101,7 +101,30 @@ const DevMenu: Screen<"DevMenu"> = ({ navigation }) => {
             </NativeItem>
 
             <NativeItem
-              onPress={() => navigation.navigate("NoteReaction")}
+              onPress={() => navigation.navigate("GradeReaction", {
+                grade: {
+                  id: "mmmhm",
+                  subjectName: "Éval TS",
+                  description: "Note de test",
+                  timestamp: 1735996215000,
+                  average: {
+                    value: 0.2
+                  },
+                  coefficient: 40,
+                  min: {
+                    value: 0
+                  },
+                  max: {
+                    value: 20
+                  },
+                  student: {
+                    value: 0
+                  },
+                  outOf: {
+                    value: 20
+                  }
+                }
+              })}
             >
               <NativeText>
                 NoteReaction
