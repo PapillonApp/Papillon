fc42458c7cf5f8550547df238621c0dc71c73830
𝕂𝕪𝕝𝕚𝕒𝕟 <kylian.jarry@scaramouch.com>
2025-01-25 23:03:08 +0100

    feat: update folder `android`

--- android/app/src/main/java/xyz/getpapillon/app/MainActivity.kt
+++ android/app/src/main/java/xyz/getpapillon/app/MainActivity.kt
@@ -1,3 +1,4 @@
+import expo.modules.splashscreen.SplashScreenManager
 package xyz.getpapillon.app
 
 import android.os.Build
@@ -17,7 +18,10 @@ class MainActivity : ReactActivity() {
     // Set the theme to AppTheme BEFORE onCreate to support
     // coloring the background, status bar, and navigation bar.
     // This is required for expo-splash-screen.
-    setTheme(R.style.AppTheme);
+    // setTheme(R.style.AppTheme);
+    // @generated begin expo-splashscreen - expo prebuild (DO NOT MODIFY) sync-f3ff59a738c56c9a6119210cb55f0b613eb8b6af
+    SplashScreenManager.registerOnActivity(this)
+    // @generated end expo-splashscreen
     super.onCreate(null); // Null is important, it prevent fragments to be restored and cause app crash
   }
 
