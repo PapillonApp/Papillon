bf56d9853bd89123b036d8640acd3b172334b230
SupRMinecraftien <superminecraftien.levrai@gmail.com>
2025-01-26 00:21:31 +0100

    feat(i18next): add no reactions titles and subtitles in English and French translations

--- src/views/settings/SettingsReactions.tsx
+++ src/views/settings/SettingsReactions.tsx
@@ -1,16 +1,21 @@
 import React from "react";
-import { ScrollView, Text, View } from "react-native";
+import { ScrollView, View } from "react-native";
 import type { Screen } from "@/router/helpers/types";
 import { useTheme } from "@react-navigation/native";
 import { useGradesStore } from "@/stores/grades";
 import ReelGallery from "@/components/Settings/ReelGallery";
 import MissingItem from "@/components/Global/MissingItem";
-import AnimatedEmoji from "@/components/Grades/AnimatedEmoji";
+import { useTranslation } from "react-i18next";
 
-const SettingsReactions: Screen<"SettingsReactions"> = () => {
+const SettingsReactions: Screen<"SettingsReactions"> = ({ navigation }) => {
   const theme = useTheme();
   const reelsObject = useGradesStore((store) => store.reels);
   const reels = Object.values(reelsObject);
+  const { t } = useTranslation();
+
+  navigation.setOptions({
+    headerTitle: t("settings.sections.general.reactions.title"),
+  });
 
   return (
     <ScrollView
@@ -29,8 +34,10 @@ const SettingsReactions: Screen<"SettingsReactions"> = () => {
         >
           <MissingItem
             animatedEmoji
-            title="Aucune réaction"
-            description="Rendez-vous dans l'onglet 'Notes' pour ajouter des réactions."
+            title={t("settings.sections.general.reactions.noReactionsTitle")}
+            description={t(
+              "settings.sections.general.reactions.noReactionsSubtitle"
+            )}
           />
         </View>
       ) : (
