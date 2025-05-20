4ea0a22de179fc2ffa9b4cffe7a98446f95650a2
Vince Linise <vincelinise@gmail.com>
2025-05-20 18:10:22 +0200

    chore: Added more dependencies

---
+++ app.config.ts
@@ -0,0 +1,50 @@
+module.exports = {
+  expo: {
+    name: "Papillon",
+    slug: "papillon",
+    version: "8.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "papillon",
+    platforms: ["ios", "android"],
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    splash: {
+      image: "./assets/images/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    ios: {
+      bundleIdentifier: "xyz.getpapillon.ios",
+      supportsTablet: true,
+    },
+    android: {
+      package: "xyz.getpapillon.app",
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      edgeToEdgeEnabled: true,
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+    plugins: [
+      "expo-router",
+      "react-native-bottom-tabs",
+      [
+        "expo-build-properties",
+        {
+          ios: {
+            useFrameworks: "static",
+          },
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+    },
+  },
+};
