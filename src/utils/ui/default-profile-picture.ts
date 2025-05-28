24ab38f59aa7c0962c5419b7dc226fdb4349d46f
Raphaël Tiphonet <43700748+Raphitpt@users.noreply.github.com>
2025-05-28 16:17:14 +0000

    fix: change default profile picture for appscho

--- src/utils/ui/default-profile-picture.ts
+++ src/utils/ui/default-profile-picture.ts
@@ -20,6 +20,7 @@ export const defaultProfilePicture = (service: AccountService, accountProvider?:
     case AccountService.Skolengo:
       return require("../../../assets/images/service_skolengo.png");
     case AccountService.Appscho:
+      return require("../../../assets/images/service_appscho.png");
     case AccountService.Local:
       return require("../../../assets/images/service_unknown.png");
     case AccountService.Izly:
