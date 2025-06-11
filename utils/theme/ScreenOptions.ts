a73a8bc957b4a42cc7ba55526f5043b81e05a1f3
Vince Linise <vincelinise@gmail.com>
2025-06-11 13:08:59 +0200

    fix: Ajout de l'option headerTransparent dans les options d'écran

--- utils/theme/ScreenOptions.ts
+++ utils/theme/ScreenOptions.ts
@@ -2,6 +2,7 @@ import { ScreenProps } from "react-native-screens";
 
 export const screenOptions: any = {
   headerLargeTitle: true,
+  headerTransparent: true,
   headerTitleStyle: {
     fontFamily: "semibold",
     fontSize: 18,
