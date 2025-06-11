e6c7959ecc2cac0b389c0f1ceb5f431f4c4c4c80
Vince Linise <vincelinise@gmail.com>
2025-06-11 19:58:30 +0200

    feat: ajouter l'importation de i18n pour la gestion des traductions

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,6 +6,7 @@ import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 import { useColorScheme } from 'react-native';
+import "@/utils/i18n";
 
 export {
   // Catch any errors thrown by the Layout component.
