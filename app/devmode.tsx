810f3832438c0d1313d010fd227cb162c26b6b6e
Tryon <lucas.lavajo@gmail.com>
2025-06-16 17:43:11 +0200

    feat: enhance DevMode with log loading functionality and improved UI elements

--- app/devmode.tsx
+++ app/devmode.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import { useTheme } from "@react-navigation/native";
+import { Plus } from "lucide-react-native";
+import React, { useEffect, useState } from "react";
 import { ScrollView, StyleSheet, Switch } from "react-native";
 
 import LogIcon from "@/components/Log/LogIcon";
@@ -14,107 +16,107 @@ export default function Devmode() {
   const accountStore = useAccountStore();
   const logsStore = useLogStore();
 
+  const { colors } = useTheme();
+
   const [showAccountStore, setShowAccountStore] = useState(false);
   const [showLogsStore, setShowLogsStore] = useState(false);
 
-    return (
-        <ScrollView
-            contentInsetAdjustmentBehavior="automatic"
-            contentContainerStyle={styles.containerContent}
-            style={styles.container}
-        >
-            <UnderConstructionNotice />
+  const [visibleLogsCount, setVisibleLogsCount] = useState(20);
 
-            <List>
-                <Item>
-                    <Trailing>
-                        <Switch
-                            style={{ marginRight: 10 }}
-                            value={showAccountStore}
-                            onValueChange={setShowAccountStore}
-                        />
-                    </Trailing>
-                    <Typography variant="title">
-                        Account Store
-                    </Typography>
-                </Item>
-                {showAccountStore && (
-                <Item>
-                    <Typography variant="body2">
-                        {JSON.stringify(accountStore, null, 2)}
-                    </Typography>
-                </Item>
-                )}
-            </List>
+  const loadMoreLogs = () => {
+    setVisibleLogsCount((prev) => prev + 20);
+  };
 
-            <List>
-                <Item>
-                    <Trailing>
-                        <Switch
-                            style={{ marginRight: 10 }}
-                            value={showLogsStore}
-                            onValueChange={setShowLogsStore}
-                        />
-                    </Trailing>
-                    <Typography variant="title">
-                        Logs Store
-                    </Typography>
-                </Item>
+  useEffect(() => {
+  if (!showLogsStore) {
+    setVisibleLogsCount(20);
+  }
 
-                {showLogsStore && (
-                    logsStore.logs.map((logEntry, index) => (
-                        <Item key={index}>
-                            <Leading>
-                                <LogIcon type={logEntry.type} />
-                            </Leading>
-                            <Typography variant="body2">
-                                {logEntry.message}
-                            </Typography>
-                            <Typography variant="caption">
-                                {logEntry.from} - {logEntry.type} - {new Date(logEntry.date).toLocaleString()}
-                            </Typography>
-                        </Item>
-                    ))
-                )}
-                <Item
-                onPress={() => {
-                    log("Log entry added from DevMode", "dfgf");
-                }}
-                >
-                    <Typography variant="title">
-                        Ajouter un LOG
-                    </Typography>
-                </Item>
-                <Item
-                onPress={() => {
-                    warn("Log entry added from DevMode", "dfgf");
-                }}
-                >
-                    <Typography variant="title">
-                        Ajouter un WARN
-                    </Typography>
-                </Item>
-                <Item
-                onPress={() => {
-                    error("Log entry added from DevMode", "dfgf");
-                }}
-                >
-                    <Typography variant="title">
-                        Ajouter une ERROR
-                    </Typography>
-                </Item>
-                
-            </List>
 
-        </ScrollView>
-    );
+}, [showLogsStore]);
+
+  return (
+    <ScrollView
+      contentInsetAdjustmentBehavior="automatic"
+      contentContainerStyle={styles.containerContent}
+      style={styles.container}
+    >
+      <UnderConstructionNotice />
+
+      <List>
+        <Item>
+          <Trailing>
+            <Switch
+              style={{ marginRight: 10 }}
+              value={showAccountStore}
+              onValueChange={setShowAccountStore}
+            />
+          </Trailing>
+          <Typography variant="title">Account Store</Typography>
+        </Item>
+        {showAccountStore && (
+          <Item>
+            <Typography variant="body2">
+              {JSON.stringify(accountStore, null, 2)}
+            </Typography>
+          </Item>
+        )}
+      </List>
+
+      <List>
+        <Item>
+          <Trailing>
+            <Switch
+              style={{ marginRight: 10 }}
+              value={showLogsStore}
+              onValueChange={setShowLogsStore}
+            />
+          </Trailing>
+          <Typography variant="title">Logs Store</Typography>
+        </Item>
+
+        {showLogsStore &&
+          logsStore.logs.slice(0, visibleLogsCount).map((logEntry, index) => (
+            <Item key={index}>
+              <Leading>
+                <LogIcon type={logEntry.type} />
+              </Leading>
+              <Typography variant="body2">{logEntry.message}</Typography>
+              <Typography variant="caption">
+                {logEntry.from} - {logEntry.type} - {new Date(logEntry.date).toLocaleString()}
+              </Typography>
+            </Item>
+          ))}
+
+        {showLogsStore && visibleLogsCount < logsStore.logs.length && (
+          <Item onPress={loadMoreLogs}>
+            <Leading>
+                <Plus color={colors.text} size={24} />
+            </Leading>
+            <Typography variant="title">Charger plus</Typography>
+          </Item>
+        )}
+      </List>
+
+      <List>
+        <Item onPress={() => log("Log entry added from DevMode", "dfgf")}>
+          <Typography variant="title">Ajouter un LOG</Typography>
+        </Item>
+        <Item onPress={() => warn("Log entry added from DevMode", "dfgf")}>
+          <Typography variant="title">Ajouter un WARN</Typography>
+        </Item>
+        <Item onPress={() => error("Log entry added from DevMode", "dfgf")}>
+          <Typography variant="title">Ajouter une ERROR</Typography>
+        </Item>
+      </List>
+    </ScrollView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 16
-    },
-    containerContent: {
-    }
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  containerContent: {},
 });
