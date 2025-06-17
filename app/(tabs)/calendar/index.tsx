a8085e19e0223d9a9d97cf1c79b18dec72dc50fa
raphckrman <41128238+raphckrman@users.noreply.github.com>
2025-06-17 04:13:40 +0200

    feat: add Course component to display schedule in TabOneScreen

--- app/(tabs)/calendar/index.tsx
+++ app/(tabs)/calendar/index.tsx
@@ -1,7 +1,9 @@
 import { Link } from "expo-router";
+import { Hamburger } from "lucide-react-native";
 import React, { ScrollView, StyleSheet, Text, View } from "react-native";
 
 import UnderConstructionNotice from "@/components/UnderConstructionNotice";
+import Course from "@/ui/components/Course";
 
 export default function TabOneScreen() {
     return (
@@ -37,6 +39,55 @@ export default function TabOneScreen() {
                     >Ouvrir une modal</Text>
                 </View>
             </Link>
+
+            <View style={{ width: "100%", display: "flex", flexDirection: "column", gap: 12 }}>
+                <Course
+                    name="Traitement des données"
+                    teacher={{
+                        firstName: "Lucas",
+                        lastName: "Martin"
+                    }}
+                    room="Bât. 12 amphi 4"
+                    color="#0095D6"
+                    status={{ label: "Prof. absent", canceled: false }}
+                    variant="primary"
+                    start={1750126049}
+                    end={1750129649}
+                />
+                <Course
+                    name="Pause méridienne"
+                    variant="separator"
+                    leading={Hamburger}
+                    start={1750126049}
+                    end={1750133249}
+                />
+                <Course
+                    name="Anglais"
+                    teacher={{
+                        firstName: "Raphaël",
+                        lastName: "Schröder"
+                    }}
+                    room="Bât. 9 salle 6"
+                    color="#21A467"
+                    status={{ label: "Professeur absent", canceled: true }}
+                    variant="primary"
+                    start={1750126049}
+                    end={1750129649}
+                />
+                <Course
+                    name="Développement web"
+                    teacher={{
+                        firstName: "Vince",
+                        lastName: "Linise"
+                    }}
+                    room="Bât. 10 salle 16"
+                    color="#21A467"
+                    status={{ label: "Cours magistral", canceled: false }}
+                    variant="primary"
+                    start={1750126049}
+                    end={1750130549}
+                />
+            </View>
         </ScrollView>
     );
 }
