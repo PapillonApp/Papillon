fb1c160e7ecfb977bfc8791aa47bcbfd45c7d35f
Vince Linise <vincelinise@gmail.com>
2025-06-24 18:00:47 +0200

    feat: ajout de la configuration ESLint et Prettier, ainsi que des plugins pour le tri des imports et la gestion des imports inutilisés

--- utils/theme/ScreenOptions.ts
+++ utils/theme/ScreenOptions.ts
@@ -1,5 +1,4 @@
 import { Platform } from "react-native";
-import { ScreenProps } from "react-native-screens";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const screenOptions: any = {
