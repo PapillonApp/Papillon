ad0bdcd716fd1843ee2fa767411a7dad5209d4fb
raphckrman <41128238+raphckrman@users.noreply.github.com>
2025-07-14 11:59:47 +0200

    feat: add animations

--- ui/utils/Animation.ts
+++ ui/utils/Animation.ts
@@ -1,18 +1,22 @@
-import { Easing } from "react-native-reanimated";
+import { Easing, FadeInUp, FadeOut } from "react-native-reanimated";
 
 const SPRING_CONFIG = { mass: 1, damping: 20, stiffness: 300 };
 
-type AnimationStyle = "default" | "spring" | "list";
+type AnimationStyle = "default" | "spring" | "list" | "fade";
 
 const PapillonSpring = (a: any) =>
-  a
-    ?.springify()
-    .mass(SPRING_CONFIG.mass)
-    .damping(SPRING_CONFIG.damping)
-    .stiffness(SPRING_CONFIG.stiffness);
+  a?.springify().mass(SPRING_CONFIG.mass).damping(SPRING_CONFIG.damping).stiffness(SPRING_CONFIG.stiffness);
 
 const PapillonList = (a: any) =>
   a?.duration(300).easing(Easing.out(Easing.exp));
+
+const PapillonFade = {
+  in: FadeInUp.duration(200).easing(Easing.out(Easing.ease)).withInitialValues({
+    opacity: 0,
+    transform: [{ translateY: 6 }],
+  }),
+  out: FadeOut.duration(150).easing(Easing.in(Easing.ease)),
+};
 
 export const Animation = (animation?: any, style?: AnimationStyle) => {
   switch (style) {
@@ -24,3 +28,6 @@ export const Animation = (animation?: any, style?: AnimationStyle) => {
     return PapillonSpring(animation);
   }
 };
+
+export const PapillonFadeIn = PapillonFade.in;
+export const PapillonFadeOut = PapillonFade.out;
