dfb8a6c21be738f485a0a1238b858963159e54e3
raphckrman <41128238+raphckrman@users.noreply.github.com>
2025-07-15 17:11:00 +0200

    feat(account): add network connectivity check in AccountManager refresh logic

--- services/shared/index.ts
+++ services/shared/index.ts
@@ -1,20 +1,26 @@
 import { Account } from "@/stores/account/types";
 import { SchoolServicePlugin } from "@/services/shared/types";
 import { Services } from "@/stores/account/types";
+import * as Network from "expo-network";
 import { error } from "@/utils/logger/logger";
 
 export class AccountManager {
   constructor(private account: Account) {}
 
   async refreshAllAccounts(): Promise<void> {
-    for (const service of this.account.services) {
-      const plugin = this.getServicePlugin(service.serviceId);
-      if (plugin) {
-        await plugin.refreshAccount(service.id);
-      }
+    const networkState = Network.useNetworkState()
+    if (networkState.isInternetReachable) {
+      for (const service of this.account.services) {
+        const plugin = this.getServicePlugin(service.serviceId);
+        if (plugin) {
+          await plugin.refreshAccount(service.id);
+        }
 
-      error("Unable to find the plugin for this ServiceAccount: " + service.serviceId, "AccountManager.refreshAllAccounts");
+        error("Unable to find the plugin for this ServiceAccount: " + service.serviceId, "AccountManager.refreshAllAccounts");
+      }
     }
+
+    error("Your device is not connected to the internet, please check your connection.", "AccountManager.refreshAllAccounts");
   }
 
   private getServicePlugin(service: Services): SchoolServicePlugin | null {
