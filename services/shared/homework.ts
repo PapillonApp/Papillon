efe1582055e25c9ae540ea2aeb66faa05621ff9c
raphckrman <41128238+raphckrman@users.noreply.github.com>
2025-07-16 12:16:58 +0200

    feat(services): add Homework interface and ReturnFormat enum

---
+++ services/shared/homework.ts
@@ -0,0 +1,32 @@
+import { Attachment } from "@/services/shared/attachment";
+
+/**
+ *  This interface defines the general structure of a homework object.
+ *  @property {string} id - Unique Identifier for each item.
+ *  @property {string} subject - The subject of the homework.
+ *  @property {string} content - The content or description of the homework in HTML.
+ *  @property {Date} dueDate - The date when the homework is due.
+ *  @property {boolean} isDone - Indicates whether the homework has been completed.
+ *  @property {ReturnFormat} returnFormat - The format in which the homework should be returned.
+ *  @property {Array<Attachment>} attachments - List of attachments related to the homework.
+ *  @property {boolean} evaluation - Indicates whether the homework is subject to evaluation.
+ *  @property {boolean} custom - Indicates whether the homework is custom or predefined.
+ *  *  @property {string} createdByAccount - The local account ID of the user who created the homework, useful for the manager.
+ */
+export interface Homework {
+  id: string;
+  subject: string;
+  content: string;
+  dueDate: Date;
+  isDone: boolean;
+  returnFormat: ReturnFormat;
+  attachments: Array<Attachment>
+  evaluation: boolean;
+  custom: boolean;
+  createdByAccount: string;
+}
+
+export enum ReturnFormat {
+  PAPER,
+  FILE_UPLOAD
+}
