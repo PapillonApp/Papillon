cad078c8348131a7f51b7bb6218a77b517f88e99
Tryon <68423470+tryon-dev@users.noreply.github.com>
2025-07-20 22:07:09 +0200

    fix(babel): change the preset babel plugin

--- babel.config.ts
+++ babel.config.ts
@@ -1,5 +1,5 @@
 module.exports = {
-  presets: ["module:metro-react-native-babel-preset"],
+  presets: ["babel-preset-expo"],
   plugins: [
     "react-native-reanimated/plugin",
     ["@babel/plugin-proposal-decorators", { legacy: true }],
