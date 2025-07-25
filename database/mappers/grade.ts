110e47f057b8305821f68951403922bf5addfff8
raphckrman <41128238+raphckrman@users.noreply.github.com>
2025-07-25 19:41:02 +0200

    feat(watermelon): add mappers to "clean" the code

---
+++ database/mappers/grade.ts
@@ -0,0 +1,45 @@
+import { mapSubjectToShared } from "@/database/mappers/subject";
+import { Grade, Period, PeriodGrades } from "@/database/models/Grades";
+import { Attachment } from "@/services/shared/attachment";
+import { Grade as SharedGrade, Period as SharedPeriod, PeriodGrades as SharedPeriodGrades } from "@/services/shared/grade";
+
+export function mapPeriodToShared(period: Period): SharedPeriod {
+  return {
+    name: period.name,
+    id: period.id,
+    start: new Date(period.start),
+    end: new Date(period.end),
+    createdByAccount: period.createdByAccount,
+    fromCache: true
+  }
+}
+
+export function mapGradeToShared(grade: Grade): SharedGrade {
+  return {
+    id: grade.id,
+    subjectId: grade.subjectId ?? "",
+    description: grade.description,
+    givenAt: new Date(grade.givenAt),
+    subjectFile: JSON.parse(grade.subjectFile ?? "") as Attachment,
+    correctionFile: JSON.parse(grade.correctionFile ?? "") as Attachment,
+    bonus: grade.bonus,
+    optional: grade.optional,
+    outOf: grade.outOf(),
+    coefficient: grade.coefficient,
+    studentScore: grade.studentScore(),
+    averageScore: grade.averageScore(),
+    minScore: grade.minScore(),
+    maxScore: grade.maxScore(),
+    fromCache: true,
+    createdByAccount: grade.createdByAccount
+  }
+}
+
+export function mapPeriodGradesToShared(data: PeriodGrades): SharedPeriodGrades {
+  return {
+    studentOverall: data.studentOverall(),
+    classAverage: data.classAverage(),
+    subjects: data.subjects.map(mapSubjectToShared),
+    createdByAccount: data.createdByAccount
+  }
+}
---
+++ database/mappers/subject.ts
@@ -0,0 +1,16 @@
+import { mapGradeToShared } from "@/database/mappers/grade";
+import Subject from "@/database/models/Subject";
+import { Subject as SharedSubject } from "@/services/shared/grade";
+
+export function mapSubjectToShared(subject: Subject): SharedSubject {
+  return {
+    id: subject.subjectId,
+    name: subject.name,
+    studentAverage: subject.studentAverage,
+    classAverage: subject.classAverage,
+    maximum: subject.maximum,
+    minimum: subject.minimum,
+    outOf: subject.outOf,
+    grades: subject.grades.map(mapGradeToShared)
+  }
+}
