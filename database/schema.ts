bfa2ac676509d973d4430e1beda104163722a238
raphckrman <41128238+raphckrman@users.noreply.github.com>
2025-07-25 20:24:06 +0200

    chore: bump watermelon schema version

--- database/schema.ts
+++ database/schema.ts
@@ -1,7 +1,7 @@
 import { appSchema, tableSchema } from '@nozbe/watermelondb';
 
 export const mySchema = appSchema({
-  version: 9,
+  version: 10,
   tables: [
     tableSchema({
       name: 'events',
