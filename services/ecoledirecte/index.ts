4e518e83d5d537b9ecc1dea7eb324e1859478c4e
raphckrman <41128238+raphckrman@users.noreply.github.com>
2025-08-04 18:15:54 +0200

    feat(ed): implementation of grades, news and homeworks methods

--- services/ecoledirecte/index.ts
+++ services/ecoledirecte/index.ts
@@ -2,6 +2,13 @@ import { Auth, Services } from "@/stores/account/types";
 import { Capabilities, SchoolServicePlugin } from "../shared/types";
 import { refreshEDAccount } from "./refresh";
 import { Account, Session } from "pawdirecte";
+import { Homework } from "../shared/homework";
+import { error } from "@/utils/logger/logger";
+import { fetchEDHomeworks } from "./homework";
+import { News } from "../shared/news";
+import { fetchEDNews } from "./news";
+import { Period, PeriodGrades } from "../shared/grade";
+import { fetchEDGradePeriods, fetchEDGrades } from "./grades";
 
 export class EcoleDirecte implements SchoolServicePlugin {
 	displayName = "EcoleDirecte";
@@ -31,5 +38,36 @@ export class EcoleDirecte implements SchoolServicePlugin {
 		this.session = refresh.session
 
 		return this;
+	}
+
+	async getHomeworks(weekNumber: number): Promise<Homework[]> {
+		if (this.session && this.account) {
+			return fetchEDHomeworks(this.session, this.account, this.accountId, weekNumber);
+		}
+		error("Session or account is not valid", "EcoleDirecte.getHomeworks")
+	}
+
+	async getNews(): Promise<News[]> {
+		if (this.session && this.account) {
+			return fetchEDNews(this.session, this.account, this.accountId);
+		}
+
+		error("Session or account is not valid", "EcoleDirecte.getNews");
+	}
+
+	async getGradesForPeriod(period: Period): Promise<PeriodGrades> {
+		if (this.session && this.account) {
+			return fetchEDGrades(this.session, this.account, this.accountId, period)
+		}
+		
+		error("Session or account is not valid", "EcoleDirecte.getGradesForPeriod");
+	}
+
+	async getGradesPeriods(): Promise<Period[]> {
+		if (this.session && this.account) {
+			return fetchEDGradePeriods(this.session, this.account, this.accountId)
+		}
+		
+		error("Session or account is not valid", "EcoleDirecte.getGradesPeriods");
 	}
 }
