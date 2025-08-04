9180db89a7eb27556a14d3393337b17672a163c5
raphckrman <41128238+raphckrman@users.noreply.github.com>
2025-08-04 18:15:15 +0200

    feat(ed): get news method

---
+++ services/ecoledirecte/news.ts
@@ -0,0 +1,16 @@
+import { Account, Session, studentHomepageTimeline } from "pawdirecte";
+import { News } from "../shared/news";
+
+export async function fetchEDNews(session: Session, account: Account, accountId: string): Promise<News[]> {
+	const news = await studentHomepageTimeline(session, account)
+	return news.map(item => ({
+		id: item.id,
+		createdAt: item.creationDate,
+		createdByAccount: accountId,
+		acknowledged: true,
+		attachments: [],
+		content: item.content,
+		author: item.authorName,
+		category: "Actualités"
+	}))
+}
