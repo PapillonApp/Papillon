711b744ee87bcd79c40b79228df1de929b34c4d4
raphckrman <41128238+raphckrman@users.noreply.github.com>
2025-08-07 01:12:53 +0200

    feat(skolengo): add parent support for grades

---
+++ services/skolengo/kid.ts
@@ -0,0 +1,14 @@
+import { Skolengo } from "skolengojs";
+import { Kid } from "../shared/kid";
+
+export function fetchSkolengoKids(session: Skolengo, accountId: string): Kid[] {
+	return (session.kids ?? []).map(kid => ({
+		id: kid.userId,
+		firstName: kid.firstName,
+		lastName: kid.lastName,
+		class: kid.className,
+		dateOfBirth: kid.dateOfBirth,
+		createdByAccount: accountId,
+		ref: kid
+	}))
+}
