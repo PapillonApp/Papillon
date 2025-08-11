b40096c8d19f74bd0689d1905f338b5647f4dcba
raphckrman <41128238+raphckrman@users.noreply.github.com>
2025-08-11 13:38:34 +0200

    feat(onboarding): add screen changements  text & background

--- app/(onboarding)/serviceSelection.tsx
+++ app/(onboarding)/serviceSelection.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, Image, Dimensions, ScrollView } from 'react-native';
 import { useTheme } from '@react-navigation/native';
-import { router } from 'expo-router';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import LottieView from 'lottie-react-native';
+import Animated, { useSharedValue, useAnimatedStyle, withTiming } from 'react-native-reanimated';
 
 import Button from '@/ui/components/Button';
 import Typography from '@/ui/components/Typography';
@@ -12,6 +12,7 @@ import Stack from '@/ui/components/Stack';
 import * as Papicons from '@getpapillon/papicons';
 import Icon from '@/ui/components/Icon';
 import { log } from '@/utils/logger/logger';
+import AnimatedNumber from '@/ui/components/AnimatedNumber';
 
 
 const { width } = Dimensions.get('window');
@@ -21,6 +22,52 @@ export default function WelcomeScreen() {
     const { colors } = theme;
     const insets = useSafeAreaInsets();
     const animation = React.useRef<LottieView>(null);
+    const [step, setStep] = useState<number>(1);
+    const [prevTextValue, setPrevText] = useState<string>("Sélectionne ton service scolaire");
+    const [nextTextValue, setNextText] = useState<string>("Comment souhaites-tu te connecter ?");
+    const [backgroundColor, setBackgroundColor] = useState<string>("#D51A67");
+
+    const animatedBackgroundColor = useSharedValue("#D51A67");
+    const prevText = useSharedValue(1);
+    const nextText = useSharedValue(0);
+
+    React.useEffect(() => {
+        animatedBackgroundColor.value = withTiming(backgroundColor, { duration: 500 });
+    }, [backgroundColor]);
+
+    function handleStepChange(newStep: number, newText: string, duration = 300) {
+        if (newText !== prevTextValue) {
+            setStep(newStep)
+            prevText.value = withTiming(0, { duration: duration });
+            nextText.value = withTiming(1, { duration: duration });
+            setNextText(newText)
+            setTimeout(() => {
+                setPrevText(newText);
+                setTimeout(() => {
+                    prevText.value = 1;
+                    nextText.value = 0;
+                }, 10);
+            }, duration);
+        }
+    }
+
+    const animatedBackgroundStyle = useAnimatedStyle(() => {
+        return {
+            backgroundColor: animatedBackgroundColor.value,
+        };
+    });
+
+    const animatedPrevTextStyle = useAnimatedStyle(() => {
+        return {
+            opacity: prevText.value,
+        };
+    });
+
+    const animatedNextTextStyle = useAnimatedStyle(() => {
+        return {
+            opacity: nextText.value,
+        };
+    });
 
     const services = [
         {
@@ -29,6 +76,8 @@ export default function WelcomeScreen() {
             type: "main",
             image: <Image source={require("@/assets/images/service_pronote.png")} style={{ width: 32, height: 32 }} />,
             onPress: () => {
+                handleStepChange(2, "Comment souhaites-tu te connecter ?");
+                setBackgroundColor("#E37900");
                 log("Pronote login");
             },
             variant: 'service' as const,
@@ -40,6 +89,7 @@ export default function WelcomeScreen() {
             type: "main",
             image: <Image source={require("@/assets/images/service_ed.png")} style={{ width: 32, height: 32 }} />,
             onPress: () => {
+                setBackgroundColor("#E37900");
                 log("EcoleDirecte login");
             },
             variant: 'service' as const,
@@ -51,6 +101,7 @@ export default function WelcomeScreen() {
             type: "main",
             image: <Image source={require("@/assets/images/service_skolengo.png")} style={{ width: 32, height: 32 }} />,
             onPress: () => {
+                setBackgroundColor("#E37900");
                 log("Skolengo login");
             },
             variant: 'service' as const,
@@ -66,6 +117,8 @@ export default function WelcomeScreen() {
                 </Icon>
             ),
             onPress: () => {
+                handleStepChange(1, "Sélectionne ton service universitaire");
+                setBackgroundColor("#000000");
                 log("University login");
             },
             variant: 'primary' as const,
@@ -76,65 +129,110 @@ export default function WelcomeScreen() {
 
     return (
         <View style={[styles.container, { backgroundColor: colors.background }]}>
-            <Stack
-                padding={28}
-                backgroundColor='#D51A67'
-                gap={30}
-                style={{
-                    alignItems: 'center',
-                    justifyContent: 'flex-start',
-                    paddingTop: insets.top + 20,
-                    borderBottomLeftRadius: 50,
-                    borderBottomRightRadius: 50,
-                    paddingBottom: 30,
-                    borderCurve: "continuous"
-                }}
+            <Animated.View
+                style={[
+                    {
+                        padding: 28,
+                        gap: 50,
+                        alignItems: 'center',
+                        justifyContent: 'flex-end',
+                        paddingTop: insets.top + 40,
+                        borderBottomLeftRadius: 50,
+                        borderBottomRightRadius: 50,
+                        paddingBottom: 30,
+                        borderCurve: "continuous",
+                        flex: 1
+                    },
+                    animatedBackgroundStyle
+                ]}
             >
-                <LottieView
-                    autoPlay
-                    loop={false}
-                    ref={animation}
-                    style={{
-                        width: 200,
-                        height: 200,
-                    }}
-                    source={require('@/assets/lotties/school-services.json')}
-                />
+                {step === 1 && (
+                    <LottieView
+                        autoPlay
+                        loop={false}
+                        ref={animation}
+                        style={{
+                            width: 200,
+                            height: 200,
+                        }}
+                        source={require('@/assets/lotties/school-services.json')}
+                    />
+                )}
+                {step === 2 && (
+                    <LottieView
+                        autoPlay
+                        loop={false}
+                        ref={animation}
+                        style={{
+                            width: 200,
+                            height: 200,
+                        }}
+                        source={require('@/assets/lotties/connexion.json')}
+                    />
+                )}
                 <Stack
-                    flex
                     vAlign='start'
                     hAlign='start'
                     width="100%"
                     gap={10}
                 >
-                    <Typography
-                        variant='h3'
-                        color='white'
+                    <Stack
+                        direction="horizontal"
+                        gap={5}
+                        style={{ alignItems: 'baseline' }}
                     >
-                        Etape 1 <Typography color='white' variant='h3' style={{ opacity: 0.65 }}>sur 3</Typography>
-                    </Typography>
-                    <Typography
-                        variant="h1"
-                        style={{ color: "white", fontSize: 34 }}
-                    >
-                        Sélectionne ton service scolaire
-                    </Typography>
+                        <Typography
+                            variant='h3'
+                            color='white'
+                        >
+                            Etape
+                        </Typography>
+                        <AnimatedNumber variant='h3' color='white'>
+                            {step}
+                        </AnimatedNumber>
+                        <Typography color='white' variant='h3' style={{ opacity: 0.65 }}>sur 3</Typography>
+                    </Stack>
+                    <View>
+                        <Animated.View
+                            style={[animatedPrevTextStyle, { position: 'absolute' }]}
+                        >
+                            <Typography
+                                variant="h1"
+                                style={{ color: "white", fontSize: 34 }}
+                            >
+                                {prevTextValue}
+                            </Typography>
+                        </Animated.View>
+                        <Animated.View
+                            style={animatedNextTextStyle}
+                        >
+                            <Typography
+                                variant="h1"
+                                style={{ color: "white", fontSize: 34 }}
+                            >
+                                {nextTextValue}
+                            </Typography>
+                        </Animated.View>
+                    </View>
                 </Stack>
-            </Stack>
+            </Animated.View>
             <ScrollView
+                style={{ flex: 1 }}
                 contentContainerStyle={{
-                    paddingBottom: insets.bottom + 20,
                     padding: 20,
                     gap: 10,
+                    flexGrow: 1
                 }}
                 showsVerticalScrollIndicator={false}
             >
-                {services.map((service, index) => {
+                {services.map((service) => {
+                    const index = services.findIndex(s => s.name === service.name);
                     const isFirstOther = service.type === "other" && services.findIndex(s => s.type === "other") === index;
                     return (
-                        <React.Fragment key={index}>
+                        <React.Fragment key={service.name}>
                             {isFirstOther && (
                                 <Stack
+                                    flex
                                     direction="horizontal"
                                     gap={8}
                                     style={{ alignItems: 'center', marginVertical: 10, marginHorizontal: 70 }}
@@ -170,7 +268,6 @@ export default function WelcomeScreen() {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: "space-between"
+        flex: 1
     },
 });
