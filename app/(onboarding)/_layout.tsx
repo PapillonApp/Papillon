317fb7353cdb10319ee6d1edadf391d76a7447b2
raphckrman <41128238+raphckrman@users.noreply.github.com>
2025-08-14 14:56:39 +0200

    feat(onboarding): add new layout, allow parameters to school/method

--- app/(onboarding)/_layout.tsx
+++ app/(onboarding)/_layout.tsx
@@ -1,41 +1,52 @@
 import React from 'react';
-import { useTheme } from '@react-navigation/native';
-import { runsIOS26 } from '@/ui/utils/IsLiquidGlass';
 
 import { screenOptions } from "@/utils/theme/ScreenOptions";
-import { useTranslation } from 'react-i18next';
 import { Stack } from '@/utils/native/AnimatedNavigator';
 import Transition from 'react-native-screen-transitions';
 import { View } from 'react-native';
+import { Services } from '@/stores/account/types';
 
 export default function OnboardingLayout() {
-  const theme = useTheme();
+    const newScreenOptions = React.useMemo(() => ({
+        ...screenOptions,
+        headerShown: false,
+        headerBackVisible: true,
+        headerTitle: '',
+        gestureEnabled: false,
+        headerTransparent: true,
+        headerTintColor: "#FFFFFF",
+        headerBackButtonDisplayMode: "minimal",
+        headerBackButtonMenuEnabled: false,
+        ...Transition.presets.ElasticCard(),
+        gesturesEnabled: false,
+        gestureDirection: []
+    }), []);
 
-  const { t } = useTranslation();
-
-  const newScreenOptions = React.useMemo(() => ({
-    ...screenOptions,
-    headerShown: false,
-    headerBackVisible: true,
-    headerTitle: '',
-    gestureEnabled: false,
-    headerTransparent: true,
-    headerTintColor: "#FFFFFF",
-    headerBackButtonDisplayMode: "minimal",
-    headerBackButtonMenuEnabled: false,
-    ...Transition.presets.ElasticCard(),
-    gesturesEnabled: false,
-    gestureDirection: []
-  }), []);
-
-  return (
-    <View style={{ flex: 1, backgroundColor: "black" }}>
-      <Stack>
-        <Stack.Screen name="welcome" options={{ ...newScreenOptions }} />
-        <Stack.Screen name="serviceSelection" options={{ ...newScreenOptions }} />
-        <Stack.Screen name="pronote/method" options={{ ...newScreenOptions }} />
-        <Stack.Screen name="university/method" options={{ ...newScreenOptions }} />
-      </Stack>
-    </View>
-  );
+    return (
+        <View style={{ flex: 1, backgroundColor: "black" }}>
+            <Stack>
+                <Stack.Screen
+                    name="welcome"
+                    options={{ ...newScreenOptions }}
+                />
+                <Stack.Screen
+                    name="serviceSelection"
+                    options={{ ...newScreenOptions }}
+                />
+                <Stack.Screen
+                    name="school/method"
+                    options={{ ...newScreenOptions }}
+                    initialParams={{ service: Services.PRONOTE }}
+                />
+                <Stack.Screen
+                    name="university/method"
+                    options={{ ...newScreenOptions }}
+                />
+                <Stack.Screen
+                    name="pronote/url"
+                    options={{ ...newScreenOptions }}
+                />
+            </Stack>
+        </View>
+    );
 }
