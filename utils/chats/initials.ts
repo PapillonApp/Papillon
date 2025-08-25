e2370834e9d3ef0dae6044a4776be511a150349e
raphckrman <41128238+raphckrman@users.noreply.github.com>
2025-08-25 13:03:16 +0200

    fix: regex doesn't detect properly initials

--- utils/chats/initials.ts
+++ utils/chats/initials.ts
@@ -1,6 +1,5 @@
 export function getInitials(name: string): string {
-  let rgx = /(\p{L})\p{L}+/gu;
-
+  let rgx = /(\p{L})\p{L}*|\p{L}/gu;
   let matches = [...name.matchAll(rgx)];
 
   let initials = (
