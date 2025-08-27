deab52aa38e8dba7ac56820d55ad8f474adc3d4f
Tryon <68423470+tryon-dev@users.noreply.github.com>
2025-08-27 15:19:53 +0200

    feat(qrcode): enhance QR login flow with modals and improved state management

--- app/(onboarding)/pronote/qrcode.tsx
+++ app/(onboarding)/pronote/qrcode.tsx
@@ -1,17 +1,129 @@
 /* eslint-disable react/react-in-jsx-scope */
-import { CameraView, useCameraPermissions } from "expo-camera"
-import { router } from "expo-router";
-import { AuthenticateError, createSessionHandle, loginQrCode, SecurityError } from "pawnote";
-import { useEffect, useState } from "react";
-import { TextInput } from "react-native";
 
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, Text, View, StyleSheet, Modal, KeyboardAvoidingView, TextInput, Keyboard, Pressable } from "react-native";
+import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context";
+import { useTheme } from "@react-navigation/native";
+import { CameraView, useCameraPermissions } from "expo-camera";
+import MaskedView from "@react-native-masked-view/masked-view";
+import * as Haptics from "expo-haptics";
+
+import Reanimated, { LinearTransition, FadeOutUp, FadeInUp } from "react-native-reanimated";
+
+import { router, useGlobalSearchParams } from "expo-router";
+import { AuthenticateError, createSessionHandle, loginQrCode, SecurityError } from "pawnote";
+import uuid from "@/utils/uuid/uuid";
 import { useAccountStore } from "@/stores/account";
 import { Services } from "@/stores/account/types";
+import Button from "@/ui/components/Button";
+import Icon from "@/ui/components/Icon";
+import { Papicons } from "@getpapillon/papicons";
 import Typography from "@/ui/components/Typography";
-import uuid from "@/utils/uuid/uuid";
+
 export default function PronoteLoginWithQR() {
+  const theme = useTheme();
+  const insets = useSafeAreaInsets();
+
+  const { colors } = theme;
   const [permission, requestPermission] = useCameraPermissions();
-  const [pinCode, setPinCode] = useState<string>();
+  const [scanned, setScanned] = useState(false);
+
+  const [keyboardOpen, setKeyboardOpen] = useState(false);
+
+  const [QRValidationCode, setQRValidationCode] = useState("");
+  const [pinModalVisible, setPinModalVisible] = useState(false);
+
+  const [loadingModalVisible, setLoadingModalVisible] = useState(false);
+
+  const codeInput = React.createRef<TextInput>();
+  const [QRData, setQRData] = useState<string | null>(null);
+
+  async function loginQR() {
+    setScanned(false);
+    setLoadingModalVisible(true);
+
+    if (QRValidationCode === "" || QRValidationCode.length !== 4) {
+      // Handle error
+      return;
+    }
+
+    const accountID = uuid();
+
+    try {
+      let decodedJSON = JSON.parse(QRData!);
+
+      const data = {
+        jeton: decodedJSON.jeton,
+        login: decodedJSON.login,
+        url: decodedJSON.url,
+      };
+
+      const session = createSessionHandle();
+      const refresh = await loginQrCode(session, {
+        qr: data,
+        pin: QRValidationCode,
+        deviceUUID: accountID
+      }).catch((error) => {
+        if (error instanceof SecurityError && !error.handle.shouldCustomPassword && !error.handle.shouldCustomDoubleAuth) {
+          router.push({
+            pathname: "/(onboarding)/pronote/2fa",
+            params: {
+              error: JSON.stringify(error),
+              session: JSON.stringify(session),
+              deviceId: accountID
+            }
+          });
+        } else {
+          throw error;
+        }
+      });
+
+      if (!refresh) throw AuthenticateError;
+
+      const user = session.user.resources[0];
+      const schoolName = user.establishmentName;
+      const className = user.className;
+
+      useAccountStore.getState().addAccount({
+        id: accountID,
+        firstName: user.name.split(" ")[0],
+        lastName: user.name.split(" ")[1],
+        schoolName,
+        className,
+        services: [{
+          id: accountID,
+          auth: {
+            accessToken: refresh.token,
+            refreshToken: refresh.token,
+            additionals: {
+              instanceURL: refresh.url,
+              kind: refresh.kind,
+              username: refresh.username,
+              deviceUUID: accountID
+            }
+          },
+          serviceId: Services.PRONOTE,
+          createdAt: (new Date()).toISOString(),
+          updatedAt: (new Date()).toISOString()
+        }],
+        createdAt: (new Date()).toISOString(),
+        updatedAt: (new Date()).toISOString()
+      });
+
+      setTimeout(() => {
+        setLoadingModalVisible(false);
+        router.push({
+          pathname: "../end/color",
+          params: {
+            accountId: accountID
+          }
+        });
+      }, 1000);
+    } catch (error) {
+      console.error(error);
+      // Handle error
+    }
+  }
 
   useEffect(() => {
     if (!permission?.granted) {
@@ -19,88 +131,345 @@ export default function PronoteLoginWithQR() {
     }
   }, [permission?.granted, requestPermission]);
 
-  console.log(permission?.granted)
+  const handleBarCodeScanned = ({
+    type,
+    data,
+  }: {
+    type: string;
+    data: string;
+  }) => {
+    setScanned(true);
+    Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
+    setQRData(data);
+    setPinModalVisible(true);
+  };
+
+  useEffect(() => {
+    if (!pinModalVisible) {
+      setScanned(false);
+      setQRData(null);
+    }
+  }, [pinModalVisible]);
+
+  const keyboardDidShow = () => setKeyboardOpen(true);
+  const keyboardDidHide = () => setKeyboardOpen(false);
+
+  useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener("keyboardDidShow", keyboardDidShow);
+    const keyboardDidHideListener = Keyboard.addListener("keyboardDidHide", keyboardDidHide);
+
+    return () => {
+      keyboardDidShowListener?.remove();
+      keyboardDidHideListener?.remove();
+    };
+  }, []);
+
+  const local = useGlobalSearchParams();
+
   return (
-    <>
-      <TextInput placeholder="PIN" value={pinCode} onChangeText={setPinCode} style={{ paddingTop: 150 }} />
-      {permission?.granted && (
-        <>
-          <Typography>Camera</Typography>
+    <SafeAreaView style={styles.container}>
+      <Modal
+        visible={loadingModalVisible}
+        animationType="slide"
+        presentationStyle="pageSheet"
+      >
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: colors.background,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <View style={{ flex: 1 }} />
+
+          <ActivityIndicator
+            size="large"
+          />
+
+          <Typography
+            style={{
+              fontSize: 18,
+              fontWeight: "600",
+              color: colors.text,
+              marginTop: 16,
+            }}
+          >
+            Connexion en cours...
+          </Typography>
+
+          <Typography
+            style={{
+              fontSize: 16,
+              fontWeight: "400",
+              color: colors.text + "80",
+              marginTop: 4,
+            }}
+          >
+            Cela peut prendre quelques instants...
+          </Typography>
+
+          <View style={{ flex: 1 }} />
+
+          <View
+            style={{
+              width: "100%",
+              paddingHorizontal: 16,
+              paddingBottom: insets.bottom,
+              gap: 8,
+            }}
+          >
+            <Button
+              title="Annuler"
+              onPress={() => {
+                setLoadingModalVisible(false);
+                router.back();
+              }}
+            />
+          </View>
+        </View>
+      </Modal>
+
+      <Modal
+        visible={pinModalVisible}
+        animationType="slide"
+        presentationStyle="pageSheet"
+        onRequestClose={() => {
+          setPinModalVisible(!pinModalVisible);
+        }}
+      >
+        <KeyboardAvoidingView
+          style={{
+            flex: 1,
+            backgroundColor: colors.background,
+          }}
+          behavior="padding"
+          keyboardVerticalOffset={insets.top}
+        >
+          <View
+            style={{
+              justifyContent: "center",
+              alignItems: "center",
+              marginBottom: 24,
+              paddingVertical: 16,
+              paddingHorizontal: 20,
+              borderBottomColor: colors.border,
+              borderBottomWidth: 0.5,
+            }}
+          >
+            <Typography
+              style={{
+                fontSize: 17,
+                fontWeight: "600",
+                color: colors.text,
+                textAlign: "center",
+              }}
+            >
+              Validation du QR code
+            </Typography>
+          </View>
+
+          <Reanimated.View
+            entering={FadeInUp.duration(250)}
+            exiting={FadeOutUp.duration(150)}
+            style={{
+              zIndex: 9999,
+              paddingTop: keyboardOpen ? 20 : 100,
+              alignItems: "center",
+            }}
+            layout={LinearTransition}
+          >
+            <Typography style={{
+              color: colors.text,
+              fontSize: 16,
+              textAlign: "center",
+              marginHorizontal: 24,
+              fontWeight: "400",
+              width: 300,
+              marginBottom: 12
+            }}>
+              Indique le code à 4 chiffres que tu viens de créer sur PRONOTE
+            </Typography>
+          </Reanimated.View>
+
+          <View
+            style={{
+              flex: 1,
+              alignItems: "center",
+              marginBottom: "7%",
+            }}
+          >
+            <TextInput
+              style={{
+                width: "90%",
+                paddingHorizontal: 20,
+                paddingVertical: 15,
+                backgroundColor: colors.card,
+                borderRadius: 12,
+                fontSize: 24,
+                color: colors.text,
+                textAlign: "center",
+                borderColor: colors.border,
+                borderWidth: 2,
+                fontWeight: "500",
+              }}
+              placeholderTextColor={colors.text + "80"}
+              placeholder="••••"
+              keyboardType="number-pad"
+              maxLength={4}
+              secureTextEntry
+              value={QRValidationCode}
+              onChangeText={(text) => setQRValidationCode(text)}
+              ref={codeInput}
+              autoFocus
+            />
+          </View>
+
+          <View
+            style={{
+              width: "100%",
+              paddingHorizontal: 16,
+              paddingBottom: insets.bottom + 16,
+              gap: 8,
+            }}
+          >
+            <Button
+              title="Confirmer"
+              onPress={() => {
+                setPinModalVisible(false);
+                loginQR();
+              }}
+            />
+            <Button
+              title="Annuler"
+              variant="outline"
+              onPress={() => {
+                setPinModalVisible(false);
+              }}
+            />
+          </View>
+        </KeyboardAvoidingView>
+      </Modal>
+
+      <View style={[styles.explainations,
+      { top: insets.top + 48 + 10 }
+      ]}>
+        <Icon size={40} fill={"white"} papicon>
+          <Papicons name="QrCode" />
+        </Icon>
+        <Typography style={styles.title}>
+          Connexion à PRONOTE
+        </Typography>
+        <Typography style={styles.text}>
+          Scanne le QR code de ton établissement pour te connecter.
+        </Typography>
+      </View>
+
+      <MaskedView
+        style={StyleSheet.absoluteFillObject}
+        maskElement={
+          <View style={styles.maskContainer}>
+            <View style={styles.transparentSquare} />
+          </View>
+        }
+      >
+        <View
+          style={styles.maskContainer}
+        />
+        {permission?.granted && (
           <CameraView
             facing="back"
-            barcodeScannerSettings={
-              {
-                barcodeTypes: ["qr"]
-              }
+            barcodeScannerSettings={{ barcodeTypes: ["qr"] }}
+            onBarcodeScanned={
+              scanned ? undefined : handleBarCodeScanned
             }
-            onBarcodeScanned={async ({ data }) => {
-              const device = uuid();
-              const decodedJSON = JSON.parse(data);
-              const formattedData = {
-                jeton: decodedJSON.jeton,
-                login: decodedJSON.login,
-                url: decodedJSON.url,
-              };
-
-              const session = createSessionHandle();
-              const authentification = await loginQrCode(session, {
-                qr: formattedData,
-                pin: pinCode ?? "",
-                deviceUUID: device
-              }).catch((error) => {
-                if (error instanceof SecurityError && !error.handle.shouldCustomPassword && !error.handle.shouldCustomDoubleAuth) {
-                  router.push({
-                    pathname: "/(onboarding)/pronote/2fa",
-                    params: {
-                      error: JSON.stringify(error),
-                      session: JSON.stringify(session),
-                      deviceId: device
-                    }
-                  })
-                } else {
-                  throw error;
-                }
-              });
-
-              if (!authentification) { throw AuthenticateError; }
-              const schoolName = session.user.resources[0].establishmentName
-              const className = session.user.resources[0].className;
-              useAccountStore.getState().addAccount({
-                id: device,
-                firstName: session.user.name.split(" ")[0],
-                lastName: session.user.name.split(" ")[1],
-                schoolName,
-                className,
-                services: [{
-                  id: device,
-                  auth: {
-                    accessToken: authentification.token,
-                    refreshToken: authentification.token,
-                    additionals: {
-                      instanceURL: authentification.url,
-                      kind: authentification.kind,
-                      username: authentification.username,
-                      deviceUUID: device
-                    }
-                  },
-                  serviceId: Services.PRONOTE,
-                  createdAt: (new Date()).toISOString(),
-                  updatedAt: (new Date()).toISOString()
-                }],
-                createdAt: (new Date()).toISOString(),
-                updatedAt: (new Date()).toISOString()
-              });
-
-              return router.push({
-                pathname: "../end/color",
-                params: {
-                  accountId: device
-                }
-              });
-            }}
+            style={StyleSheet.absoluteFillObject}
           />
-        </>
-      )}
-    </>
-  )
+        )}
+        {permission?.granted && (
+          <View style={styles.transparentSquareBorder} />
+        )}
+      </MaskedView>
+      <Pressable
+        onPress={() => router.back()}
+        style={[
+          styles.backButton,
+          { top: insets.top + 4 }
+        ]}
+      >
+        <Icon size={26} fill={"#FFFFFF"} papicon>
+          <Papicons name={"ArrowLeft"} />
+        </Icon>
+      </Pressable>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "black",
+  },
+  maskContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.3)",
+  },
+  transparentSquare: {
+    position: "absolute",
+    width: 300,
+    height: 300,
+    backgroundColor: "black",
+    borderWidth: 2,
+    borderColor: "#fff",
+    borderRadius: 30,
+    borderCurve: "continuous",
+    alignSelf: "center",
+    top: "35%",
+  },
+  backButton: {
+    position: 'absolute',
+    left: 16,
+    zIndex: 200,
+    backgroundColor: '#ffffff42',
+    padding: 10,
+    borderRadius: 100,
+  },
+  transparentSquareBorder: {
+    position: "absolute",
+    width: 300,
+    height: 300,
+    backgroundColor: "transparent",
+    borderWidth: 2,
+    borderColor: "#fff",
+    borderRadius: 30,
+    borderCurve: "continuous",
+    alignSelf: "center",
+    top: "35%",
+  },
+
+  explainations: {
+    position: "absolute",
+    alignItems: "center",
+    justifyContent: "center",
+    marginHorizontal: 24,
+    gap: 4,
+    zIndex: 9999,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "600",
+    color: "white",
+    textAlign: "center",
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: "400",
+    color: "white",
+    textAlign: "center",
+    opacity: 0.8,
+  },
+});
