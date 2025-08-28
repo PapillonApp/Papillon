390a6401448a5e47a257ad45d5c63ce579f5255e
raphckrman <41128238+raphckrman@users.noreply.github.com>
2025-08-28 03:00:44 +0200

    fix: remove useless routes

--- app/(tabs)/profile/_layout.tsx
+++ app/(tabs)/profile/_layout.tsx
@@ -17,29 +17,6 @@ export default function Layout() {
           headerTransparent: true,
         }}
       />
-      <Stack.Screen
-        name="cards"
-        options={{
-          headerShown: true,
-          headerTitle: "Cartes",
-          headerLargeTitle: false,
-          headerTransparent: false,
-        }}
-      />
-      <Stack.Screen
-        name="attendance"
-        options={{
-          headerShown: true,
-          headerLargeTitle: false,
-          headerTransparent: true,
-          presentation: "modal",
-        }}
-        initialParams={{
-          periods: [],
-          currentPeriod: {},
-          attendances: []
-        }}
-      />
     </Stack>
   );
 }
