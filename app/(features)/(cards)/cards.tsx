cd89ca9f782ffc62acce6909915f44ac115f1296
Vince Linise <contact@vincelinise.com>
2025-08-31 19:12:40 +0200

    feat: enhance QR code page with dynamic background color and responsive size fix: improve profile picture condition check in Tabs component refactor: update QRCodeAndCardsPage for better loading state management and animations

--- app/(features)/(cards)/cards.tsx
+++ app/(features)/(cards)/cards.tsx
@@ -2,24 +2,31 @@ import { getManager } from "@/services/shared";
 import { Balance } from "@/services/shared/balance";
 import { useAccountStore } from "@/stores/account";
 import { Services } from "@/stores/account/types";
+import AnimatedPressable from "@/ui/components/AnimatedPressable";
 import Button from "@/ui/components/Button";
+import { Dynamic } from "@/ui/components/Dynamic";
 import Icon from "@/ui/components/Icon";
 import { NativeHeaderPressable, NativeHeaderSide, NativeHeaderTitle } from "@/ui/components/NativeHeader";
 import Stack from "@/ui/components/Stack";
 import Typography from "@/ui/components/Typography";
+import { PapillonAppearIn, PapillonAppearOut } from "@/ui/utils/Transition";
 import { getServiceBackground, getServiceLogo, getServiceName } from "@/utils/services/helper";
 import { Papicons, Plus } from "@getpapillon/papicons";
+import { useTheme } from "@react-navigation/native";
 import { LinearGradient } from "expo-linear-gradient";
 import { router } from "expo-router";
 import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Image, Pressable, View } from "react-native";
+import { ActivityIndicator, Image, Pressable, View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 export { getServiceName } from "@/utils/services/helper"
 
 export default function QRCodeAndCardsPage() {
   const [wallets, setWallets] = useState<Balance[]>([]);
   const accounts = useAccountStore((state) => state.accounts);
   const lastUsedAccount = useAccountStore((state) => state.lastUsedAccount);
+
+  const [loadingWallets, setLoadingWallets] = useState(true);
 
   const account = accounts.find((a) => a.id === lastUsedAccount);
   const selfCompatible = account?.services.filter(
@@ -34,6 +41,7 @@ export default function QRCodeAndCardsPage() {
       result.push(balance)
     }
     setWallets(result);
+    setLoadingWallets(false);
   }
 
   useEffect(() => {
@@ -46,19 +54,28 @@ export default function QRCodeAndCardsPage() {
   const pileHeight = cardHeight + (wallets.length - 1) * cardOffset;
   const { t } = useTranslation();
 
+  const { colors } = useTheme();
+
   return (
     <>
-      <View style={{ padding: 20, flex: 1 }}>
+      <ScrollView style={{ padding: 20, flex: 1 }}>
         {selfCompatible && selfCompatible?.length > 0 ? (
           <>
             <View style={{ position: "relative", height: pileHeight }}>
               {wallets.map((c, i) => (
-                <Card
+                <Dynamic
+                  animated
                   key={c.createdByAccount + c.label}
-                  index={i}
-                  wallet={c}
-                  service={account?.services.find(service => service.id === c.createdByAccount)?.serviceId ?? Services.TURBOSELF}
-                />
+                  entering={PapillonAppearIn}
+                  exiting={PapillonAppearOut}
+                >
+                  <Card
+                    key={c.createdByAccount + c.label}
+                    index={i}
+                    wallet={c}
+                    service={account?.services.find(service => service.id === c.createdByAccount)?.serviceId ?? Services.TURBOSELF}
+                  />
+                </Dynamic>
               ))}
             </View>
 
@@ -108,7 +125,7 @@ export default function QRCodeAndCardsPage() {
             }} />
           </Stack>
         )}
-      </View>
+      </ScrollView>
 
       <NativeHeaderSide side="Left">
         <NativeHeaderPressable onPress={() => router.back()}>
@@ -141,7 +158,7 @@ export function Card({
   const offset = index * 60;
 
   return (
-    <Pressable
+    <AnimatedPressable
       onPress={() => {
         if (!disabled) {
           router.push({
@@ -152,7 +169,7 @@ export function Card({
       }}
       style={{
         width: "100%",
-        minHeight: 235,
+        minHeight: 210,
         borderRadius: 25,
         overflow: "hidden",
         transform: [{ translateY: offset }],
@@ -172,7 +189,7 @@ export function Card({
           right: 0,
           left: 0,
           width: "100%",
-          height: 245,
+          height: 215,
         }}
         resizeMode="cover"
       />
@@ -220,6 +237,6 @@ export function Card({
           </Stack>
         </Stack>
       </View>
-    </Pressable>
+    </AnimatedPressable>
   )
 }
