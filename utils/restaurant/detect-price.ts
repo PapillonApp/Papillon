963557c22226076c43fc900a572c04505f1ec587
raphckrman <41128238+raphckrman@users.noreply.github.com>
2025-09-06 01:49:53 +0200

    feat(ard): detect meal price

---
+++ utils/restaurant/detect-price.ts
@@ -0,0 +1,22 @@
+import { CanteenHistoryItem } from "@/services/shared/canteen";
+
+export function detectMealPrice (history: CanteenHistoryItem[]): number | null {
+  let mostFrequentAmount: number = 0;
+  let maxCount = 0;
+  const amountCount: Record<number, number> = {};
+
+  for (const consumption of history) {
+    if (consumption.amount < 0) {
+      const amount = consumption.amount * -1;
+
+      amountCount[amount] = (amountCount[amount] || 0) + 1;
+
+      if (amountCount[amount] > maxCount) {
+        maxCount = amountCount[amount];
+        mostFrequentAmount = amount;
+      }
+    }
+  }
+
+  return mostFrequentAmount;
+}
