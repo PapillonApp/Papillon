fdf989ba755c41b8f76ad37bdf052945f117e38f
Louis Lesniak <louis@lesniak.fr>
2025-09-05 23:21:25 +0200

    Supprime tous les fichiers du dépôt

--- ui/utils/Duration.ts
+++
@@ -1,7 +0,0 @@
-export function formatDuration(seconds: number): string {
-  const hours = Math.floor(seconds / 3600);
-  const minutes = Math.floor((seconds % 3600) / 60);
-  if (hours && minutes) {return `${hours}h ${minutes} ${minutes > 1 ? "mins" : "min"}`;}
-  if (hours) {return `${hours} ${hours > 1 ? "heures" : "heure"}`;}
-  return `${minutes} ${minutes > 1 ? "mins" : "min"}`;
-}
--- ui/utils/IsLiquidGlass.ts
+++
@@ -1,6 +0,0 @@
-import { Platform } from "react-native";
-
-export const runsIOS26 = () => {
-  return false;
-  // return (Platform.OS === "ios" && parseInt(Platform.Version) >= 26);
-}
