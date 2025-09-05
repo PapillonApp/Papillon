aecb4bf130959a121ed8ade96d0b5dd9ee2949a5
raphckrman <41128238+raphckrman@users.noreply.github.com>
2025-09-05 19:30:24 +0200

    fix: lint turboself

--- services/turboself/balance.ts
+++ services/turboself/balance.ts
@@ -1,15 +1,16 @@
 import { Client } from "turboself-api";
+
 import { Balance } from "../shared/balance";
 
 export async function fetchTurboSelfBalance(session: Client, accountId: string): Promise<Balance[]> {
-	const balances = await session.getBalances()
-	const lunchPrice = (session.host?.lunchPrice ?? 0)
-	return balances.map(balance => ({
-		amount: balance.amount,
-		currency: session.establishment?.currencySymbol ?? "€",
-		lunchRemaining: lunchPrice > 0 ? Math.floor(balance.estimatedAmount / lunchPrice) : 0,
-		lunchPrice,
-		label: balance.label,
-		createdByAccount: accountId
-	}))
+  const balances = await session.getBalances()
+  const lunchPrice = (session.host?.lunchPrice ?? 0)
+  return balances.map(balance => ({
+    amount: balance.amount,
+    currency: session.establishment?.currencySymbol ?? "€",
+    lunchRemaining: lunchPrice > 0 ? Math.floor(balance.estimatedAmount / lunchPrice) : 0,
+    lunchPrice,
+    label: balance.label,
+    createdByAccount: accountId
+  }))
 }
--- services/turboself/qrcode.ts
+++ services/turboself/qrcode.ts
@@ -1,18 +1,19 @@
 import { Client } from "turboself-api";
+
 import { QRCode, QRType } from "../shared/canteen";
 
 export function fetchTurboSelfQRCode(session: Client, accountId: string): QRCode {
-	if (session.host?.cardNumber) {
-		return {
-			type: QRType.QRCode,
-			data: String(session.host.cardNumber),
-			createdByAccount: accountId
-		};
-	}
+  if (session.host?.cardNumber) {
+    return {
+      type: QRType.QRCode,
+      data: String(session.host.cardNumber),
+      createdByAccount: accountId
+    };
+  }
 	
-	return {
-		type: QRType.QRCode,
-		data: "",
-		createdByAccount: accountId
-	}
+  return {
+    type: QRType.QRCode,
+    data: "",
+    createdByAccount: accountId
+  }
 }
