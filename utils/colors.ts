9043ee47d55713cb0ff56b64ae37f052bb017d4f
Vince Linise <contact@vincelinise.com>
2025-09-07 00:16:04 +0200

    fix: Nom des couleurs

--- utils/colors.ts
+++ utils/colors.ts
@@ -1,3 +1,5 @@
+import { t } from "i18next";
+
 export enum Colors {
   PINK,
   YELLOW,
@@ -11,37 +13,37 @@ export const AppColors = [
   {
     mainColor: "#DD007D",
     backgroundColor: "#FAD9EC",
-    nameKey: "colors.rose",
+    nameKey: t("Colors.rose"),
     colorEnum: Colors.PINK,
   },
   {
     mainColor: "#E8B048",
     backgroundColor: "#FCF3E4",
-    nameKey: "colors.yellow",
+    nameKey: t("Colors.yellow"),
     colorEnum: Colors.YELLOW,
   },
   {
     mainColor: "#26B290",
     backgroundColor: "#DEF3EE",
-    nameKey: "colors.green",
+    nameKey: t("Colors.green"),
     colorEnum: Colors.GREEN,
   },
   {
     mainColor: "#C400DD",
     backgroundColor: "#F6D9FA",
-    nameKey: "colors.purple",
+    nameKey: t("Colors.purple"),
     colorEnum: Colors.PURPLE,
   },
   {
     mainColor: "#48B7E8",
     backgroundColor: "#E4F4FC",
-    nameKey: "colors.blue",
+    nameKey: t("Colors.blue"),
     colorEnum: Colors.BLUE,
   },
   {
     mainColor: "#6D6D6D",
     backgroundColor: "#E9E9E9",
-    nameKey: "colors.black",
+    nameKey: t("Colors.black"),
     colorEnum: Colors.BLACK,
   },
 ];
