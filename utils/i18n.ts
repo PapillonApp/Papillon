a3fcb22fc60b21cfbb512bc5397e39c15dcf0a48
Vince Linise <contact@vincelinise.com>
2025-09-13 18:13:47 +0200

    i18n: Added Spanish support

--- utils/i18n.ts
+++ utils/i18n.ts
@@ -5,11 +5,13 @@ import { initReactI18next } from "react-i18next";
 import en from "@/locales/en.json";
 import fr from "@/locales/fr.json";
 import de from "@/locales/de.json";
+import es from "@/locales/es.json";
 
 const resources = {
   fr: { translation: fr },
   en: { translation: en },
   de: { translation: de },
+  es: { translation: es },
 };
 
 const languageDetector = {
