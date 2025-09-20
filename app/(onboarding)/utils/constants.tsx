31056ce02a95b61a144d8287ab1438dfdbda59b4
Tom Theret <tomnils.theret@gmail.com>
2025-09-20 23:08:31 +0200

    chore(lint): constants

--- app/(onboarding)/utils/constants.tsx
+++ app/(onboarding)/utils/constants.tsx
@@ -18,80 +18,80 @@ export interface SupportedService {
 }
 
 export function GetSupportedServices(redirect: (path: { pathname: string, options?: UnknownInputParams }) => void): SupportedService[] {
-    const theme = useTheme();
-    const { colors } = theme;
-    const { t } = useTranslation()
+  const theme = useTheme();
+  const { colors } = theme;
+  const { t } = useTranslation()
 
-    return [
-        {
-            name: "pronote",
-            title: "PRONOTE",
-            type: "main",
-            image: require("@/assets/images/service_pronote.png"),
-            onPress: () => {
-                redirect({ pathname: './school/method', options: { service: Services.PRONOTE } });
-            },
-            variant: 'service' as const,
-            color: 'light' as const,
-        },
-        {
-            name: "ed",
-            title: "ÉcoleDirecte",
-            type: "main",
-            image: require("@/assets/images/service_ed.png"),
-            onPress: () => {
-                redirect({ pathname: './ecoledirecte/credentials', options: { service: Services.ECOLEDIRECTE } });
-            },
-            variant: 'service' as const,
-            color: 'light' as const,
-        },
-        {
-            name: "skolengo",
-            title: "Skolengo",
-            type: "main",
-            image: require("@/assets/images/service_skolengo.png"),
-            onPress: () => {
-                redirect({ pathname: './school/method', options: { service: Services.SKOLENGO } });
-            },
-            variant: 'service' as const,
-            color: 'light' as const,
-        },
-        {
-            name: "separator",
-            title: "separator",
-            type: "separator",
-            image: require("@/assets/images/service_skolengo.png"),
-            onPress: () => {
+  return [
+    {
+      name: "pronote",
+      title: "PRONOTE",
+      type: "main",
+      image: require("@/assets/images/service_pronote.png"),
+      onPress: () => {
+        redirect({ pathname: './school/method', options: { service: Services.PRONOTE } });
+      },
+      variant: 'service' as const,
+      color: 'light' as const,
+    },
+    {
+      name: "ed",
+      title: "ÉcoleDirecte",
+      type: "main",
+      image: require("@/assets/images/service_ed.png"),
+      onPress: () => {
+        redirect({ pathname: './ecoledirecte/credentials', options: { service: Services.ECOLEDIRECTE } });
+      },
+      variant: 'service' as const,
+      color: 'light' as const,
+    },
+    {
+      name: "skolengo",
+      title: "Skolengo",
+      type: "main",
+      image: require("@/assets/images/service_skolengo.png"),
+      onPress: () => {
+        redirect({ pathname: './school/method', options: { service: Services.SKOLENGO } });
+      },
+      variant: 'service' as const,
+      color: 'light' as const,
+    },
+    {
+      name: "separator",
+      title: "separator",
+      type: "separator",
+      image: require("@/assets/images/service_skolengo.png"),
+      onPress: () => {
 
-            },
-            variant: 'service' as const,
-            color: 'light' as const,
-        },
-        {
-            name: "university",
-            title: t("ONBOARDING_UNIVERSITY"),
-            type: "other",
-            icon: <Papicons name={"Star"} />,
-            onPress: () => {
-                redirect({ pathname: './university/method' });
+      },
+      variant: 'service' as const,
+      color: 'light' as const,
+    },
+    {
+      name: "university",
+      title: t("ONBOARDING_UNIVERSITY"),
+      type: "other",
+      icon: <Papicons name={"Star"} />,
+      onPress: () => {
+        redirect({ pathname: './university/method' });
 
-            },
-            variant: 'primary' as const,
-            style: { backgroundColor: theme.dark ? colors.border : "black" },
-        },
-        {
-            name: "university",
-            title: t("ONBOARDING_RESTAURANTS"),
-            type: "other",
-            icon: <Papicons name={"Cutlery"} />,
-            onPress: () => {
-                redirect({ pathname: './restaurants/method' });
+      },
+      variant: 'primary' as const,
+      style: { backgroundColor: theme.dark ? colors.border : "black" },
+    },
+    {
+      name: "university",
+      title: t("ONBOARDING_RESTAURANTS"),
+      type: "other",
+      icon: <Papicons name={"Cutlery"} />,
+      onPress: () => {
+        redirect({ pathname: './restaurants/method' });
 
-            },
-            variant: 'primary' as const,
-            color: 'light' as const
-        }
-    ]
+      },
+      variant: 'primary' as const,
+      color: 'light' as const
+    }
+  ]
 }
 
 export interface SupportedUniversity {
@@ -104,91 +104,91 @@ export interface SupportedUniversity {
 }
 
 export function GetSupportedUniversities(redirect: (path: { pathname: string }) => void): SupportedUniversity[] {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    return [
-        {
-            name: "univ-lorraine",
-            title: "Université de Lorraine",
-            hasLimitedSupport: false,
-            image: require("@/assets/images/univ_lorraine.png"),
-            type: "main",
-            onPress: () => {
-                redirect({ pathname: './multi/credentials', options: { color: "#000000", university: "ULorraine", url: "https://mobile-back.univ-lorraine.fr" } });
+  return [
+    {
+      name: "univ-lorraine",
+      title: "Université de Lorraine",
+      hasLimitedSupport: false,
+      image: require("@/assets/images/univ_lorraine.png"),
+      type: "main",
+      onPress: () => {
+        redirect({ pathname: './multi/credentials', options: { color: "#000000", university: "ULorraine", url: "https://mobile-back.univ-lorraine.fr" } });
 
-            },
-        },
-        {
-            name: "univ-nimes",
-            title: "Université de Nîmes",
-            hasLimitedSupport: false,
-            image: require("@/assets/images/univ_nimes.png"),
-            type: "main",
-            onPress: () => {
-                redirect({ pathname: './multi/credentials', options: { color: "#FF341B", university: "UNîmes", url: "https://mobile-back.unimes.fr" } });
+      },
+    },
+    {
+      name: "univ-nimes",
+      title: "Université de Nîmes",
+      hasLimitedSupport: false,
+      image: require("@/assets/images/univ_nimes.png"),
+      type: "main",
+      onPress: () => {
+        redirect({ pathname: './multi/credentials', options: { color: "#FF341B", university: "UNîmes", url: "https://mobile-back.unimes.fr" } });
 
-            },
-        },
-        {
-            name: "univ-uphf",
-            title: "Université Polytechnique Hauts-de-France",
-            hasLimitedSupport: false,
-            image: require("@/assets/images/univ_uphf.png"),
-            type: "main",
-            onPress: () => {
-                redirect({ pathname: './multi/credentials', options: { color: "#008DB0", university: "UPHF", url: "https://appmob.uphf.fr/backend" } });
+      },
+    },
+    {
+      name: "univ-uphf",
+      title: "Université Polytechnique Hauts-de-France",
+      hasLimitedSupport: false,
+      image: require("@/assets/images/univ_uphf.png"),
+      type: "main",
+      onPress: () => {
+        redirect({ pathname: './multi/credentials', options: { color: "#008DB0", university: "UPHF", url: "https://appmob.uphf.fr/backend" } });
 
-            },
-        },
-        // {
-        //     name: "iut-lannion",
-        //     title: "IUT de Lannion",
-        //     hasLimitedSupport: false,
-        //     image: require("@/assets/images/univ_lannion.png"),
-        //     type: "main",
-        //     onPress: () => { }
-        // },
-        // {
-        //     name: "limited-functions",
-        //     title: t("Feature_Limited"),
-        //     hasLimitedSupport: true,
-        //     image: require("@/assets/images/univ_lannion.png"),
-        //     type: "separator",
-        //     onPress: () => { }
-        // },
-        // {
-        //     name: "univ-rennes-1",
-        //     title: "Université de Rennes 1",
-        //     hasLimitedSupport: true,
-        //     image: require("@/assets/images/univ_rennes1.png"),
-        //     type: "main",
-        //     onPress: () => { }
-        // },
-        // {
-        //     name: "univ-rennes-2",
-        //     title: "Université de Rennes 2",
-        //     hasLimitedSupport: true,
-        //     image: require("@/assets/images/univ_rennes2.png"),
-        //     type: "main",
-        //     onPress: () => { }
-        // },
-        // {
-        //     name: "univ-limoges",
-        //     title: "Université de Limoges",
-        //     type: "main",
-        //     hasLimitedSupport: true,
-        //     image: require("@/assets/images/univ_limoges.png"),
-        //     onPress: () => { }
-        // },
-        // {
-        //     name: "univ_paris_sorbonne",
-        //     title: "Université de Sorbonne Paris Nord",
-        //     hasLimitedSupport: true,
-        //     image: require("@/assets/images/univ_paris_sorbonne.png"),
-        //     type: "main",
-        //     onPress: () => { }
-        // }
-    ]
+      },
+    },
+    /*{
+      name: "iut-lannion",
+      title: "IUT de Lannion",
+      hasLimitedSupport: false,
+      image: require("@/assets/images/univ_lannion.png"),
+      type: "main",
+      onPress: () => { }
+    },
+    {
+      name: "limited-functions",
+      title: t("Feature_Limited"),
+      hasLimitedSupport: true,
+      image: require("@/assets/images/univ_lannion.png"),
+      type: "separator",
+      onPress: () => { }
+    },
+    {
+      name: "univ-rennes-1",
+      title: "Université de Rennes 1",
+      hasLimitedSupport: true,
+      image: require("@/assets/images/univ_rennes1.png"),
+      type: "main",
+      onPress: () => { }
+    },
+    {
+      name: "univ-rennes-2",
+      title: "Université de Rennes 2",
+      hasLimitedSupport: true,
+      image: require("@/assets/images/univ_rennes2.png"),
+      type: "main",
+      onPress: () => { }
+    },
+    {
+      name: "univ-limoges",
+      title: "Université de Limoges",
+      type: "main",
+      hasLimitedSupport: true,
+      image: require("@/assets/images/univ_limoges.png"),
+      onPress: () => { }
+    },
+    {
+      name: "univ_paris_sorbonne",
+      title: "Université de Sorbonne Paris Nord",
+      hasLimitedSupport: true,
+      image: require("@/assets/images/univ_paris_sorbonne.png"),
+      type: "main",
+      onPress: () => { }
+    }*/
+  ]
 }
 
 export interface LoginMethod {
@@ -200,46 +200,46 @@ export interface LoginMethod {
 }
 
 export function GetLoginMethods(redirect: (path: { pathname: RelativePathString }) => void): LoginMethod[] {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    return [
-        {
-            id: "map",
-            availableFor: [Services.PRONOTE, Services.SKOLENGO],
-            description: t("ONBOARDING_METHOD_POSITION"),
-            icon: <Papicons name={"MapPin"} />,
-            onPress: async () => {
-                redirect({ pathname: './map' });
-            }
-        },
-        {
-            id: "search",
-            availableFor: [Services.PRONOTE, Services.SKOLENGO],
-            description: t("ONBOARDING_METHOD_SEARCH"),
-            icon: <Papicons name={"Search"} />,
-            onPress: () => {
-                redirect({ pathname: './search' })
-            }
-        },
-        {
-            id: "qrcode",
-            availableFor: [Services.PRONOTE],
-            description: t("ONBOARDING_METHOD_QRCODE"),
-            icon: <Papicons name={"QrCode"} />,
-            onPress: () => {
-                redirect({ pathname: "/(onboarding)/pronote/qrcode" });
-            }
-        },
-        {
-            id: "url",
-            availableFor: [Services.PRONOTE],
-            description: t("ONBOARDING_METHOD_LINK"),
-            icon: <Papicons name={"Link"} />,
-            onPress: () => {
-                redirect({ pathname: '../pronote/url' });
-            }
-        }
-    ]
+  return [
+    {
+      id: "map",
+      availableFor: [Services.PRONOTE, Services.SKOLENGO],
+      description: t("ONBOARDING_METHOD_POSITION"),
+      icon: <Papicons name={"MapPin"} />,
+      onPress: async () => {
+        redirect({ pathname: './map' });
+      }
+    },
+    {
+      id: "search",
+      availableFor: [Services.PRONOTE, Services.SKOLENGO],
+      description: t("ONBOARDING_METHOD_SEARCH"),
+      icon: <Papicons name={"Search"} />,
+      onPress: () => {
+        redirect({ pathname: './search' })
+      }
+    },
+    {
+      id: "qrcode",
+      availableFor: [Services.PRONOTE],
+      description: t("ONBOARDING_METHOD_QRCODE"),
+      icon: <Papicons name={"QrCode"} />,
+      onPress: () => {
+        redirect({ pathname: "/(onboarding)/pronote/qrcode" });
+      }
+    },
+    {
+      id: "url",
+      availableFor: [Services.PRONOTE],
+      description: t("ONBOARDING_METHOD_LINK"),
+      icon: <Papicons name={"Link"} />,
+      onPress: () => {
+        redirect({ pathname: '../pronote/url' });
+      }
+    }
+  ]
 }
 
 export interface SupportedRestaurant {
@@ -252,36 +252,36 @@ export interface SupportedRestaurant {
 }
 
 export function GetSupportedRestaurants(redirect: (path: { pathname: string }) => void): SupportedRestaurant[] {
-    return [
-        {
-            name: "turboself",
-            title: "TurboSelf",
-            hasLimitedSupport: false,
-            image: require("@/assets/images/turboself.png"),
-            type: "main",
-            onPress: () => {
-                redirect({ pathname: '../turboself/credentials' });
-            }
-        },
-        {
-            name: "ard",
-            title: "ARD",
-            hasLimitedSupport: false,
-            image: require("@/assets/images/ard.png"),
-            type: "main",
-            onPress: () => {
-                redirect({ pathname: '../ard/credentials' });
-            }
-        },
-        {
-            name: "izly",
-            title: "Izly",
-            hasLimitedSupport: false,
-            image: require("@/assets/images/izly.png"),
-            type: "main",
-            onPress: () => {
-                redirect({ pathname: '../izly/credentials' });
-            }
-        }
-    ]
+  return [
+    {
+      name: "turboself",
+      title: "TurboSelf",
+      hasLimitedSupport: false,
+      image: require("@/assets/images/turboself.png"),
+      type: "main",
+      onPress: () => {
+        redirect({ pathname: '../turboself/credentials' });
+      }
+    },
+    {
+      name: "ard",
+      title: "ARD",
+      hasLimitedSupport: false,
+      image: require("@/assets/images/ard.png"),
+      type: "main",
+      onPress: () => {
+        redirect({ pathname: '../ard/credentials' });
+      }
+    },
+    {
+      name: "izly",
+      title: "Izly",
+      hasLimitedSupport: false,
+      image: require("@/assets/images/izly.png"),
+      type: "main",
+      onPress: () => {
+        redirect({ pathname: '../izly/credentials' });
+      }
+    }
+  ]
 }
