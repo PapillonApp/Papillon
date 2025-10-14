8f8d936911d1786011080f957f97b673be4b2aa7
Grise <148749571+Grise3@users.noreply.github.com>
2025-10-14 08:23:57 +0000

    feat(timetable): add timezone conversion for lessons

--- utils/date.ts
+++ utils/date.ts
@@ -1,6 +1,13 @@
 import { formatDistance } from "date-fns";
 import { fr } from "date-fns/locale";
 
+export const adjustToDeviceTimeZone = (date: Date): Date => {
+  const apiOffset = 2 * 60 * 60 * 1000;
+  const localOffset = new Date().getTimezoneOffset() * 60 * 1000;
+  const totalOffset = localOffset + apiOffset;
+  return new Date(date.getTime() - totalOffset);
+};
+
 export function formatRelativeTime(date: Date): string {
   return formatDistance(date, new Date(), { 
     addSuffix: true, 
