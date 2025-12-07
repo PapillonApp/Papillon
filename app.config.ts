73f0e2b342488b4315f29de29c3364cb3e374a32
Vince Linise <contact@vincelinise.com>
2025-12-07 17:09:22 +0100

    chore(reanimated): Update to Reanimated 4.2 and enable flags

--- app.config.ts
+++ app.config.ts
@@ -32,6 +32,7 @@ module.exports = {
             CFBundleURLSchemes: ["papillon", "izly", "skoapp-prod"],
           },
         ],
+        CADisableMinimumFrameDurationOnPhone: true,
       },
       supportsTablet: true,
       config: {
