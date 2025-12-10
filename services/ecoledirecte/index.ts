9299a6ea8d6b149e83505cf8ea1daf64eb9b148e
raphckrman <41128238+raphckrman@users.noreply.github.com>
2025-12-10 23:38:44 +0100

    feat(ed): implement new timetable methods

--- services/ecoledirecte/index.ts
+++ services/ecoledirecte/index.ts
@@ -1,21 +1,15 @@
 import { Client } from "@blockshub/blocksdirecte";
 
-import { fetchEDBalances } from "@/services/ecoledirecte/balance";
-import { fetchEDQRCode } from "@/services/ecoledirecte/qrcode";
-import { Balance } from "@/services/shared/balance";
-import { QRCode } from "@/services/shared/canteen";
 import { Auth, Services } from "@/stores/account/types";
 import { error } from "@/utils/logger/logger";
 
 import { Attendance } from "../shared/attendance";
-import { Chat, Message } from "../shared/chat";
 import { Period, PeriodGrades } from "../shared/grade";
 import { Homework } from "../shared/homework";
 import { News } from "../shared/news";
 import { CourseDay } from "../shared/timetable";
 import { Capabilities, SchoolServicePlugin } from "../shared/types";
 import { fetchEDAttendance } from "./attendance";
-import { fetchEDChatMessage, fetchEDChats } from "./chat";
 import { fetchEDGradePeriods, fetchEDGrades } from "./grades";
 import { fetchEDHomeworks, setEDHomeworkAsDone } from "./homework";
 import { fetchEDNews } from "./news";
@@ -29,12 +23,9 @@ export class EcoleDirecte implements SchoolServicePlugin {
     Capabilities.REFRESH, 
     Capabilities.NEWS, 
     Capabilities.ATTENDANCE, 
-    Capabilities.CHAT_READ,
     Capabilities.GRADES,
     Capabilities.HOMEWORK,
-    Capabilities.TIMETABLE,
-    Capabilities.CANTEEN_QRCODE,
-    Capabilities.CANTEEN_BALANCE,
+    Capabilities.TIMETABLE
   ];
   session: Client | undefined;
   authData: Auth = {};
@@ -75,69 +66,34 @@ export class EcoleDirecte implements SchoolServicePlugin {
   }
 
   async getGradesPeriods(): Promise<Period[]> {
-    if (this.session && this.account) {
-      return fetchEDGradePeriods(this.session, this.account, this.accountId)
+    if (this.session) {
+      return fetchEDGradePeriods(this.session, this.accountId)
     }
 		
-    error("Session or account is not valid", "EcoleDirecte.getGradesPeriods");
+    throw error("Session or account is not valid", "EcoleDirecte.getGradesPeriods");
   }
 
   async getAttendanceForPeriod(): Promise<Attendance> {
-    if (this.session && this.account) {
-      return fetchEDAttendance(this.session, this.account, this.accountId);
+    if (this.session) {
+      return fetchEDAttendance(this.session, this.accountId);
     }
 
-    error("Session or account is not valid", "EcoleDirecte.getAttendanceForPeriod");
+    throw error("Session or account is not valid", "EcoleDirecte.getAttendanceForPeriod");
   }
 
   async getWeeklyTimetable(weekNumber: number): Promise<CourseDay[]> {
-    if (this.session && this.account) {
-      return fetchEDTimetable(this.session, this.account, this.accountId, weekNumber)
+    if (this.session) {
+      return fetchEDTimetable(this.session, this.accountId, weekNumber)
     }
 
-    error("Session or account is not valid", "EcoleDirecte.getWeeklyTimetable")
-  }
-
-  async getChats(): Promise<Chat[]> {
-    if (this.session && this.account) {
-      return fetchEDChats(this.session, this.account, this.accountId);
-    }
-
-    error("Session or account is not valid", "EcoleDirecte.getChats");
-  }
-
-  async getChatMessages(chat: Chat): Promise<Message[]> {
-    if (this.session && this.account) {
-      return fetchEDChatMessage(this.session, this.account, this.accountId, chat);
-    }
-
-    error("Session or account is not valid", "EcoleDirecte.getChats");
+    throw error("Session or account is not valid", "EcoleDirecte.getWeeklyTimetable")
   }
 
   async setHomeworkCompletion(homework: Homework, state?: boolean): Promise<Homework> {
-    if (this.session && this.account) {
-      return setEDHomeworkAsDone(this.session, this.account, homework, state)
+    if (this.session) {
+      return setEDHomeworkAsDone(this.session, homework, state)
     }
 
-    error("Session or account is not valid", "EcoleDirecte.setHomeworkCompletion");
-  }
-
-  async getCanteenQRCodes(): Promise<QRCode> {
-    if (this.session && this.account) {
-      return fetchEDQRCode(this.account)
-    }
-
-    error("Session is not valid", "EcoleDirecte.getCanteenQRCodes")
-  }
-
-  async getCanteenBalances(): Promise<Balance[]> {
-    if (this.session && this.account) {
-      return (await fetchEDBalances(this.session)).map(balance => ({
-        ...balance,
-        createdByAccount: this.accountId,
-      }))
-    }
-
-    error("Session is not valid", "EcoleDirecte.getCanteenBalances")
+    throw error("Session or account is not valid", "EcoleDirecte.setHomeworkCompletion");
   }
 }
