b4edec0e7c7a81b15692bdca9c6e5d12f7a6e5ea
LeChacal <dev.lechacal@gmail.com>
2026-01-10 17:34:42 +0100

    Removed a " "

--- app/(modals)/profile.tsx
+++ app/(modals)/profile.tsx
@@ -180,6 +180,6 @@ export default function CustomProfileScreen() {
           </NativeHeaderPressable>
         </NativeHeaderSide>
       </ScrollView>
-    </ KeyboardAvoidingView >
+    </KeyboardAvoidingView >
   );
 }
