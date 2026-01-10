9657df657808602d92a3fe0dc8e01551317fcd20
LeChacal <dev.lechacal@gmail.com>
2026-01-10 14:40:01 +0100

    Added dev .bat files

---
+++ build-dev.bat
@@ -0,0 +1,3 @@
+@echo off
+echo --- Build et Installation de Papillon Dev ---
+npx expo run:android --variant devDebug
---
+++ start-dev.bat
@@ -0,0 +1,3 @@
+@echo off
+echo --- Lancement de Papillon Dev ---
+npx expo start --dev-client
