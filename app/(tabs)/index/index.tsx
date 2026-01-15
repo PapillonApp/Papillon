49e541efaa02844278f0dc5e1f4a90f1a2072b24
LeChacal <dev.lechacal@gmail.com>
2026-01-15 16:33:37 +0100

    Edited the order of widgets
    Suggested by : @guguss-31

--- app/(tabs)/index/index.tsx
+++ app/(tabs)/index/index.tsx
@@ -39,16 +39,16 @@ const HomeScreen = () => {
 
   const data: HomeWidgetItem[] = useMemo(() => [
     {
-      icon: <Papicons name={"Grades"} />,
-      title: t("Latest_Grades"),
-      redirect: "(tabs)/grades",
-      render: renderGrades
-    },
-    {
       icon: <Papicons name={"Calendar"} />,
       title: t("Home_Widget_NextCourses"),
       redirect: "(tabs)/calendar",
       render: renderTimeTable
+    },
+    {
+      icon: <Papicons name={"Grades"} />,
+      title: t("Latest_Grades"),
+      redirect: "(tabs)/grades",
+      render: renderGrades
     },
     {
       icon: <Papicons name={"Tasks"} />,
