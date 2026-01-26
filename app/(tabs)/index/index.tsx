4b70180def83b1631e1943301d310f0a428662f2
LeChacal <dev.lechacal@gmail.com>
2026-01-26 23:14:57 +0100

    feat: added tasks and grades widgets to home

--- app/(tabs)/index/index.tsx
+++ app/(tabs)/index/index.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import Wallpaper from './atoms/Wallpaper';
-import HomeHeader from './atoms/HomeHeader';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import HomeTopBar from './atoms/HomeTopBar';
-import HomeTimeTableWidget from './widgets/timetable';
 import { Papicons } from '@getpapillon/papicons';
-import { t } from 'i18next';
 import { LegendList } from '@legendapp/list';
-import { useHomeData } from './hooks/useHomeData';
-import HomeWidget, { HomeWidgetItem } from './components/HomeWidget';
-import { useBottomTabBarHeight } from 'react-native-bottom-tabs';
 import { useRouter } from 'expo-router';
+import { t } from 'i18next';
+import React, { useMemo } from 'react';
+import { useBottomTabBarHeight } from 'react-native-bottom-tabs';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
 import { useAccountStore } from '@/stores/account';
 import { checkConsent } from '@/utils/logger/consent';
+
+import HomeHeader from './atoms/HomeHeader';
+import HomeTopBar from './atoms/HomeTopBar';
+import Wallpaper from './atoms/Wallpaper';
+import HomeWidget, { HomeWidgetItem } from './components/HomeWidget';
+import { useHomeData } from './hooks/useHomeData';
+import HomeGradesWidget from './widgets/Grades';
+import HomeTasksWidget from './widgets/Tasks';
+import HomeTimeTableWidget from './widgets/timetable';
 
 const HomeScreen = () => {
   const insets = useSafeAreaInsets();
@@ -38,28 +42,49 @@ const HomeScreen = () => {
   useHomeData();
 
   const renderTimeTable = React.useCallback(() => <HomeTimeTableWidget />, []);
+  const renderTasks = React.useCallback(() => <HomeTasksWidget />, []);
+  const renderGrades = React.useCallback(() => <HomeGradesWidget />, []);
 
-  const data: HomeWidgetItem[] = React.useMemo(() => [
+  const data: HomeWidgetItem[] = useMemo(() => [
     {
       icon: <Papicons name={"Calendar"} />,
       title: t("Home_Widget_NextCourses"),
       redirect: "(tabs)/calendar",
       render: renderTimeTable
     },
-  ], [renderTimeTable]);
+    {
+      icon: <Papicons name={"Grades"} />,
+      title: t("Latest_Grades"),
+      redirect: "(tabs)/grades",
+      render: renderGrades
+    },
+    {
+      icon: <Papicons name={"Tasks"} />,
+      title: t("Home_Widget_NewHomeworks"),
+      redirect: "(tabs)/tasks",
+      render: renderTasks
+    }
+
+  ], [renderTimeTable, renderTasks, renderGrades]);
 
   return (
     <>
       <Wallpaper />
       <HomeTopBar />
+
       <LegendList
         renderItem={({ item }) => <HomeWidget item={item} />}
         keyExtractor={(item) => item.title}
+
         ListHeaderComponent={<HomeHeader />}
         style={{ flex: 1 }}
+
+        showsVerticalScrollIndicator={false}
+
         contentContainerStyle={{
           paddingBottom: insets.bottom + bottomTabBarHeight,
           paddingHorizontal: 16,
+          gap: 12
         }}
         data={data}
       />
