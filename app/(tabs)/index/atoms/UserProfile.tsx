9e43ee189dee43d8db53db555628c68c0f0a3e0d
Vince Linise <contact@vincelinise.com>
2026-01-31 22:53:03 +0100

    feat: Contenerize account switcher

--- app/(tabs)/index/atoms/UserProfile.tsx
+++ app/(tabs)/index/atoms/UserProfile.tsx
@@ -1,10 +1,11 @@
 import { Papicons } from '@getpapillon/papicons';
 import { MenuView } from '@react-native-menu/menu';
+import { useTheme } from '@react-navigation/native';
 import { LiquidGlassView } from '@sbaiahmed1/react-native-blur';
 import { useRouter } from 'expo-router';
-import { t } from 'i18next';
 import React from 'react';
-import { StyleSheet, TouchableOpacity } from 'react-native';
+import { Dimensions, StyleSheet } from 'react-native';
+import { Pressable } from 'react-native';
 
 import Avatar from '@/ui/components/Avatar';
 import Stack from '@/ui/components/Stack';
@@ -15,6 +16,8 @@ import { useUserProfileData } from '../hooks/useUserProfileData';
 const UserProfile = ({ subtitle, onPress }: { subtitle?: string, onPress?: () => void }) => {
   const router = useRouter();
   const { firstName, lastName, level, establishment, initials, profilePicture } = useUserProfileData();
+
+  const theme = useTheme();
 
   const accountsList = [
     {
@@ -30,74 +33,83 @@ const UserProfile = ({ subtitle, onPress }: { subtitle?: string, onPress?: () =>
 
   return (
     <Stack inline flex>
-      <LiquidGlassView
-        glassType="clear"
-        isInteractive={true}
-        glassTintColor="transparent"
-        glassOpacity={0}
-        style={{
-          borderRadius: 300,
-          zIndex: 999999,
-        }}
+      <Stack
+        direction="horizontal"
+        hAlign="center"
+        gap={10}
       >
-        <MenuView
-          actions={[
-            {
-              id: 'workspaces',
-              title: '',
-              displayInline: true,
-              subactions: accountsList.map((account) => ({
-                id: account.id,
-                title: account.firstName + ' ' + account.lastName,
-                subtitle: account.establishment,
-                state: account.current ? 'on' : 'off',
-              })),
-            },
-            {
-              id: 'edit',
-              title: 'Edit profile',
-              image: 'person.crop.circle',
-              imageColor: 'white',
-            },
-            {
-              id: 'add',
-              title: 'Add account',
-              image: 'plus',
-              imageColor: 'white',
-            },
-          ]}
-        >
-          <TouchableOpacity
-            activeOpacity={0.6}
+        <Pressable onPress={() => router.push('/(modals)/profile')}>
+          <LiquidGlassView
+            glassType="clear"
+            isInteractive={true}
+            glassTintColor="transparent"
+            glassOpacity={0}
+            style={{
+              borderRadius: 300,
+              zIndex: 999999,
+            }}
           >
-            <Stack
-              direction="horizontal"
-              hAlign="center"
-              gap={14}
-            >
+            <Avatar
+              size={40}
+              initials={initials}
+              imageUrl={profilePicture}
+            />
+          </LiquidGlassView>
+        </Pressable>
 
-              <Avatar
-                size={38}
-                initials={initials}
-                imageUrl={profilePicture}
-              />
-              <Stack direction="vertical" vAlign="center" gap={0}>
-                <Stack direction="horizontal" hAlign="center" gap={6}>
-                  <Typography nowrap color='white' variant='navigation' weight='bold'>
-                    {t('Home_Welcome_Name', { name: firstName, emoji: "👋" })}
-                  </Typography>
-                  <Papicons name="chevrondown" size={20} color="white" opacity={0.5} style={{ marginRight: 12 }} />
-                </Stack>
-                {subtitle &&
-                  <Typography nowrap color='white' variant='body1' style={{ opacity: 0.7 }}>
-                    {subtitle}
-                  </Typography>
-                }
+        <LiquidGlassView
+          glassType="clear"
+          isInteractive={true}
+          glassTintColor="transparent"
+          glassOpacity={0}
+          style={{
+            borderRadius: 300,
+            zIndex: 999999,
+          }}
+        >
+          <MenuView
+            actions={[
+              {
+                id: 'workspaces',
+                title: '',
+                displayInline: true,
+                subactions: accountsList.map((account) => ({
+                  id: account.id,
+                  title: account.firstName + ' ' + account.lastName,
+                  subtitle: account.establishment,
+                  state: account.current ? 'on' : 'off',
+                })),
+              },
+              {
+                id: 'edit',
+                title: 'Edit profile',
+                image: 'person.crop.circle',
+                imageColor: theme.colors.text,
+              },
+              {
+                id: 'add',
+                title: 'Add account',
+                image: 'plus',
+                imageColor: theme.colors.text,
+              },
+            ]}
+          >
+            <Stack direction="vertical" vAlign="center" gap={0} style={{ height: 42, paddingHorizontal: 12 }}>
+              <Stack direction="horizontal" hAlign="center" gap={6}>
+                <Typography nowrap color='white' variant='navigation' weight='bold' style={{ maxWidth: Dimensions.get('window').width - 230 }}>
+                  {firstName} {lastName}
+                </Typography>
+                <Papicons name="chevrondown" size={20} color="white" opacity={0.5} style={{ marginRight: 0 }} />
               </Stack>
+              {subtitle &&
+                <Typography nowrap color='white' variant='body1' style={{ opacity: 0.7 }}>
+                  {subtitle}
+                </Typography>
+              }
             </Stack>
-          </TouchableOpacity>
-        </MenuView>
-      </LiquidGlassView>
+          </MenuView>
+        </LiquidGlassView>
+      </Stack>
     </Stack>
   );
 };
