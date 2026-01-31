f2a41939bf3754cb7627a8a27965069c14c8bcb6
LeChacal <dev.lechacal@gmail.com>
2026-01-31 17:30:52 +0100

    feat: added attendance, news, homeworks, timetable

--- services/webuntis/index.ts
+++ services/webuntis/index.ts
@@ -1,18 +1,15 @@
 import { WebUntis } from "webuntis";
 
-import { Auth,Services } from "@/stores/account/types";
+import { Auth, Services } from "@/stores/account/types";
 import { error } from "@/utils/logger/logger";
 
-import { Attendance } from "../shared/attendance";
-import { Balance } from "../shared/balance";
-import { Booking,BookingDay, CanteenHistoryItem, CanteenKind, CanteenMenu, QRCode } from "../shared/canteen";
-import { Chat, Message,Recipient } from "../shared/chat";
-import { Period, PeriodGrades } from "../shared/grade";
+import { Period } from "../shared/grade";
 import { Homework } from "../shared/homework";
-import { Kid } from "../shared/kid";
 import { News } from "../shared/news";
-import { Course, CourseDay,CourseResource } from "../shared/timetable";
+import { CourseDay } from "../shared/timetable";
 import { Capabilities, SchoolServicePlugin } from "../shared/types";
+import { getCurrentWebUntisPeriod } from "./attendance";
+import { fetchWebUntisHomeworks } from "./homework";
 import { refreshWebUntisAccount } from "./refresh";
 import { fetchWebUntisWeekTimetable } from "./timetable";
 
@@ -26,29 +23,18 @@ export class WebUntisService implements SchoolServicePlugin {
   constructor(public accountId: string) {}
 
   async refreshAccount(credentials: Auth): Promise<WebUntisService> {
-    const refresh = (await refreshWebUntisAccount(this.accountId, credentials));
+    const refresh = await refreshWebUntisAccount(this.accountId, credentials);
     this.authData = refresh.auth;
     this.session = refresh.session;
 
-    this.capabilities.push(Capabilities.TIMETABLE)
+    this.capabilities.push(Capabilities.TIMETABLE);
+    this.capabilities.push(Capabilities.HOMEWORK);
+    this.capabilities.push(Capabilities.NEWS);
 		
     return this;
   }
 
-  getKids?: (() => Kid[]) | undefined;
-  getCanteenKind?: (() => CanteenKind) | undefined;
-  getHomeworks?: ((weekNumber: number) => Promise<Homework[]>) | undefined;
-  getNews?: (() => Promise<News[]>) | undefined;
-  getGradesForPeriod?: ((period: Period, kid?: Kid) => Promise<PeriodGrades>) | undefined;
-  getGradesPeriods?: (() => Promise<Period[]>) | undefined;
-  getAttendanceForPeriod?: ((period: string) => Promise<Attendance>) | undefined;
-  getAttendancePeriods?: (() => Promise<Period[]>) | undefined;
-  getWeeklyCanteenMenu?: ((startDate: Date) => Promise<CanteenMenu[]>) | undefined;
-  getChats?: (() => Promise<Chat[]>) | undefined;
-  getChatRecipients?: ((chat: Chat) => Promise<Recipient[]>) | undefined;
-  getChatMessages?: ((chat: Chat) => Promise<Message[]>) | undefined;
-  getRecipientsAvailableForNewChat?: (() => Promise<Recipient[]>) | undefined;
-  getCourseResources?: ((course: Course) => Promise<CourseResource[]>) | undefined;
+  // Timetable
 
   async getWeeklyTimetable(weekNumber: number, date: Date): Promise<CourseDay[]> {
     await this.session?.validateSession();
@@ -58,17 +44,38 @@ export class WebUntisService implements SchoolServicePlugin {
     }
 
     error("Session is not valid", "WebUntis.getWeeklyTimetable");
-
     return [];
   }
-  sendMessageInChat?: ((chat: Chat, content: string) => Promise<void>) | undefined;
-  setNewsAsAcknowledged?: ((news: News) => Promise<News>) | undefined;
-  setHomeworkCompletion?: ((homework: Homework, state?: boolean) => Promise<Homework>) | undefined;
-  createMail?: ((subject: string, content: string, recipients: Recipient[], cc?: Recipient[], bcc?: Recipient[]) => Promise<Chat>) | undefined;
-  getCanteenBalances?: (() => Promise<Balance[]>) | undefined;
-  getCanteenTransactionsHistory?: (() => Promise<CanteenHistoryItem[]>) | undefined;
-  getCanteenQRCodes?: (() => Promise<QRCode>) | undefined;
-  getCanteenBookingWeek?: ((weekNumber: number) => Promise<BookingDay[]>) | undefined;
-  setMealAsBooked?: ((meal: Booking, booked?: boolean) => Promise<Booking>) | undefined;
 
+  // Homeworks
+
+  async getHomeworks(weekNumber: number): Promise<Homework[]> {
+    await this.session?.validateSession();
+  
+    if (this.session) {
+      return fetchWebUntisHomeworks(this.session, this.accountId, weekNumber);
+    }
+
+    error("Session is not valid", "WebUntis.getHomeworks");
+    return [];
+  }
+
+  // Attendance
+
+  async getAttendancePeriods(): Promise<Period[]> {
+    return [getCurrentWebUntisPeriod()];
+  }
+  
+  // News
+
+  async getNews(): Promise<News[]> {
+    await this.session?.validateSession();
+  
+    if (this.session) {
+      return [];
+    }
+
+    error("Session is not valid", "WebUntis.getNews");
+    return [];
+  }
 }
