a2381d9fbc07cd7c3ed24b52a6ce20357a5eb1b4
godetremy <godetremy@free.fr>
2026-02-01 19:11:42 +0100

    feat: Widget target config

---
+++ targets/widget/expo-target.config.js
@@ -0,0 +1,8 @@
+/** @type {import('@bacons/apple-targets/app.plugin').ConfigFunction} */
+module.exports = config => ({
+  type: "widget",
+  icon: "https://github.com/expo.png",
+  entitlements: {
+    "com.apple.security.application-groups": config.ios.entitlements["com.apple.security.application-groups"],
+  }
+});
