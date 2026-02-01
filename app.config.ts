e97252abbaf06e7181daac9814327129b0e8e99c
Vince Linise <contact@vincelinise.com>
2026-02-01 19:40:51 +0100

    chore(lang): Add missing localizations to plist

--- app.config.ts
+++ app.config.ts
@@ -32,7 +32,7 @@ module.exports = {
             CFBundleURLSchemes: ["papillon", "izly", "skoapp-prod"],
           },
         ],
-        CFBundleLocalizations: ["fr", "en", "br", "pt", "de", "es", "tr", "ja", "ru", "ko", "af", "ar", "el", "hi", "nl", "pl", "ro", "sq", "uk", "vi"],
+        CFBundleLocalizations: ["fr", "en", "br", "pt", "de", "es", "tr", "ja", "ru", "ko", "af", "ar", "el", "hi", "nl", "pl", "ro", "sq", "uk", "vi", "bg", "bn", "cs", "da", "fi", "he", "hu", "id", "no", "sk", "sv", "th"],
         CADisableMinimumFrameDurationOnPhone: true,
       },
       supportsTablet: true,
