79f1ebc2b1c32f701ae2408727a2bf0e97be83a7
LeChacal <dev.lechacal@gmail.com>
2026-02-09 22:29:46 +0100

    fix: wrong type for client

--- services/shared/types.ts
+++ services/shared/types.ts
@@ -6,6 +6,7 @@ import { SessionHandle } from "pawnote";
 import { Client as ArdClient } from "pawrd";
 import { Skolengo as SkolengoSession } from "skolengojs";
 import { Client as TurboselfClient } from "turboself-api";
+import { WebUntisClient } from "webuntis-client";
 
 import { Appscho } from "@/services/appscho";
 import { Lannion } from "@/services/lannion";
@@ -59,7 +60,7 @@ export interface SchoolServicePlugin {
     | TurboselfClient
     | User
     | LannionClient
-    | WebUntis
+    | WebUntisClient
     | undefined;
 
   refreshAccount: (
