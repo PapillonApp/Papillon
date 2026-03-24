d967bda8c5f267f54e21c6ecf8e44e05b5c3a65a
Vince Linise <contact@vincelinise.com>
2026-03-24 14:43:59 +0100

    fix: userprofile crash on onboarding

--- app/(tabs)/index/atoms/UserProfile.tsx
+++ app/(tabs)/index/atoms/UserProfile.tsx
@@ -15,95 +15,100 @@ import { runsIOS26 } from '@/ui/utils/IsLiquidGlass';
 import { useUserProfileData } from '../hooks/useUserProfileData';
 
 const UserProfile = ({ subtitle, onPress }: { subtitle?: string, onPress?: () => void }) => {
-  const router = useRouter();
-  const { firstName, lastName, level, establishment, initials, profilePicture } = useUserProfileData();
+  try {
+    const router = useRouter();
+    const { firstName, lastName, level, establishment, initials, profilePicture } = useUserProfileData();
 
-  const theme = useTheme();
+    const theme = useTheme();
 
-  const accountsList = [
-    {
-      firstName,
-      lastName,
-      level,
-      establishment,
-      initials,
-      profilePicture,
-      current: true,
-    },
-  ]
+    const accountsList = [
+      {
+        firstName,
+        lastName,
+        level,
+        establishment,
+        initials,
+        profilePicture,
+        current: true,
+      },
+    ]
 
-  return (
-    <Stack inline flex>
-      <Stack
-        direction="horizontal"
-        hAlign="center"
-        gap={10}
-      >
-        <Pressable onPress={() => router.push('/(modals)/profile')}>
-          <UserProfileItemContainer
-            glassType="clear"
-            isInteractive={true}
-            glassTintColor="transparent"
-            glassOpacity={0}
-            style={{
-              borderRadius: 300,
-              zIndex: 999999,
-            }}
-          >
-            <Avatar
-              size={40}
-              initials={initials}
-              imageUrl={profilePicture}
-            />
-          </UserProfileItemContainer>
-        </Pressable>
+    return (
+      <Stack inline flex>
+        <Stack
+          direction="horizontal"
+          hAlign="center"
+          gap={10}
+        >
+          <Pressable onPress={() => router.push('/(modals)/profile')}>
+            <UserProfileItemContainer
+              glassType="clear"
+              isInteractive={true}
+              glassTintColor="transparent"
+              glassOpacity={0}
+              style={{
+                borderRadius: 300,
+                zIndex: 999999,
+              }}
+            >
+              <Avatar
+                size={40}
+                initials={initials}
+                imageUrl={profilePicture}
+              />
+            </UserProfileItemContainer>
+          </Pressable>
 
-        <UserProfileItemContainer>
-          <MenuView
-            actions={[
-              {
-                id: 'workspaces',
-                title: '',
-                displayInline: true,
-                subactions: accountsList.map((account) => ({
-                  id: account.id,
-                  title: account.firstName + ' ' + account.lastName,
-                  subtitle: account.establishment,
-                  state: account.current ? 'on' : 'off',
-                })),
-              },
-              {
-                id: 'edit',
-                title: 'Edit profile',
-                image: 'person.crop.circle',
-                imageColor: theme.colors.text,
-              },
-              {
-                id: 'add',
-                title: 'Add account',
-                image: 'plus',
-                imageColor: theme.colors.text,
-              },
-            ]}
-          >
-            <Stack direction="vertical" vAlign="center" gap={0} style={{ height: 42, paddingHorizontal: 12 }}>
-              <Stack direction="horizontal" hAlign="center" gap={6}>
-                <Typography nowrap color='white' variant='navigation' weight='bold' style={{ maxWidth: Dimensions.get('window').width - 230 }}>
-                  {firstName} {lastName}
-                </Typography>
-                <Papicons name="chevrondown" size={20} color="white" opacity={0.5} style={{ marginRight: 0 }} />
+          <UserProfileItemContainer>
+            <MenuView
+              actions={[
+                {
+                  id: 'workspaces',
+                  title: '',
+                  displayInline: true,
+                  subactions: accountsList.map((account) => ({
+                    id: account.id,
+                    title: account.firstName + ' ' + account.lastName,
+                    subtitle: account.establishment,
+                    state: account.current ? 'on' : 'off',
+                  })),
+                },
+                {
+                  id: 'edit',
+                  title: 'Edit profile',
+                  image: 'person.crop.circle',
+                  imageColor: theme.colors.text,
+                },
+                {
+                  id: 'add',
+                  title: 'Add account',
+                  image: 'plus',
+                  imageColor: theme.colors.text,
+                },
+              ]}
+            >
+              <Stack direction="vertical" vAlign="center" gap={0} style={{ height: 42, paddingHorizontal: 12 }}>
+                <Stack direction="horizontal" hAlign="center" gap={6}>
+                  <Typography nowrap color='white' variant='navigation' weight='bold' style={{ maxWidth: Dimensions.get('window').width - 230 }}>
+                    {firstName} {lastName}
+                  </Typography>
+                  <Papicons name="chevrondown" size={20} color="white" opacity={0.5} style={{ marginRight: 0 }} />
+                </Stack>
+                {subtitle &&
+                  <Typography nowrap color='white' variant='body1' style={{ opacity: 0.7 }}>
+                    {subtitle}
+                  </Typography>
+                }
               </Stack>
-              {subtitle &&
-                <Typography nowrap color='white' variant='body1' style={{ opacity: 0.7 }}>
-                  {subtitle}
-                </Typography>
-              }
-            </Stack>
-          </MenuView>
-        </UserProfileItemContainer>
+            </MenuView>
+          </UserProfileItemContainer>
+        </Stack>
       </Stack>
-    </Stack>
-  );
+    );
+  }
+  catch (e) {
+    return null;
+  }
 };
 
 const UserProfileItemContainer = ({ children }: { children: React.ReactNode }) => {
@@ -130,7 +135,6 @@ const UserProfileItemContainer = ({ children }: { children: React.ReactNode }) =
       {children}
     </Stack>
   )
-
 }
 
 const styles = StyleSheet.create({
