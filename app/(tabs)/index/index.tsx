4339f77902cf9bfc16706a5b2ede8e7da3887939
LeChacal <dev.lechacal@gmail.com>
2026-04-03 17:55:39 +0200

    fixed: error on launch

--- app/(tabs)/index/index.tsx
+++ app/(tabs)/index/index.tsx
@@ -1,37 +1,36 @@
-import { Papicons } from '@getpapillon/papicons';
-import { LegendList } from '@legendapp/list';
-import { useRouter } from 'expo-router';
-import { t } from 'i18next';
-import React, { useMemo } from 'react';
-import { useBottomTabBarHeight } from 'react-native-bottom-tabs';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { Papicons } from "@getpapillon/papicons";
+import { LegendList } from "@legendapp/list";
+import { useFocusEffect, useRouter } from "expo-router";
+import { t } from "i18next";
+import React, { useMemo } from "react";
+import { useBottomTabBarHeight } from "react-native-bottom-tabs";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-import { useAccountStore } from '@/stores/account';
-import { useSettingsStore } from '@/stores/settings';
-import { checkConsent } from '@/utils/logger/consent';
+import { useAccountStore } from "@/stores/account";
+import { useSettingsStore } from "@/stores/settings";
+import { checkConsent } from "@/utils/logger/consent";
 
-import HomeHeader from './atoms/HomeHeader';
-import HomeTopBar from './atoms/HomeTopBar';
-import Wallpaper from './atoms/Wallpaper';
-import HomeWidget, { HomeWidgetItem } from './components/HomeWidget';
-import { useHomeData } from './hooks/useHomeData';
-import HomeGradesWidget from './widgets/Grades';
-import HomeTasksWidget from './widgets/Tasks';
-import HomeTimeTableWidget from './widgets/timetable';
+import HomeHeader from "./atoms/HomeHeader";
+import HomeTopBar from "./atoms/HomeTopBar";
+import Wallpaper from "./atoms/Wallpaper";
+import HomeWidget, { HomeWidgetItem } from "./components/HomeWidget";
+import { useHomeData } from "./hooks/useHomeData";
+import HomeGradesWidget from "./widgets/Grades";
+import HomeTasksWidget from "./widgets/Tasks";
+import HomeTimeTableWidget from "./widgets/timetable";
 
 const HomeScreen = () => {
   const insets = useSafeAreaInsets();
   const bottomTabBarHeight = useBottomTabBarHeight();
-  const focused = useIsFocused();
 
   // Account
   const store = useAccountStore();
-  const accounts = useAccountStore((state) => state.accounts);
+  const accounts = useAccountStore(state => state.accounts);
   const account = accounts.find(a => a.id === store.lastUsedAccount)!;
   const router = useRouter();
 
   React.useEffect(() => {
-    console.log(accounts)
+    console.log(accounts);
     if (accounts.length === 0) {
       router.replace("/(onboarding)/welcome");
     }
@@ -57,30 +56,32 @@ const HomeScreen = () => {
   const renderTasks = React.useCallback(() => <HomeTasksWidget />, []);
   const renderGrades = React.useCallback(() => <HomeGradesWidget />, []);
 
-  const data: HomeWidgetItem[] = useMemo(() => [
-    {
-      icon: <Papicons name={"Calendar"} />,
-      title: t("Home_Widget_NextCourses"),
-      redirect: "(tabs)/calendar",
-      enabled: !disabledWidgets.includes("calendar"),
-      render: renderTimeTable
-    },
-    {
-      icon: <Papicons name={"Grades"} />,
-      title: t("Home_Widget_NewGrades"),
-      redirect: "(tabs)/grades",
-      enabled: !disabledWidgets.includes("grades"),
-      render: renderGrades
-    },
-    {
-      icon: <Papicons name={"Tasks"} />,
-      title: t("Home_Widget_NewTasks"),
-      redirect: "(tabs)/tasks",
-      enabled: !disabledWidgets.includes("tasks"),
-      render: renderTasks
-    }
-
-  ], [renderTimeTable, renderTasks, renderGrades, disabledWidgets]);
+  const data: HomeWidgetItem[] = useMemo(
+    () => [
+      {
+        icon: <Papicons name={"Calendar"} />,
+        title: t("Home_Widget_NextCourses"),
+        redirect: "(tabs)/calendar",
+        enabled: !disabledWidgets.includes("calendar"),
+        render: renderTimeTable,
+      },
+      {
+        icon: <Papicons name={"Grades"} />,
+        title: t("Home_Widget_NewGrades"),
+        redirect: "(tabs)/grades",
+        enabled: !disabledWidgets.includes("grades"),
+        render: renderGrades,
+      },
+      {
+        icon: <Papicons name={"Tasks"} />,
+        title: t("Home_Widget_NewTasks"),
+        redirect: "(tabs)/tasks",
+        enabled: !disabledWidgets.includes("tasks"),
+        render: renderTasks,
+      },
+    ],
+    [renderTimeTable, renderTasks, renderGrades, disabledWidgets]
+  );
 
   return (
     <>
@@ -89,17 +90,14 @@ const HomeScreen = () => {
 
       <LegendList
         renderItem={({ item }) => <HomeWidget item={item} />}
-        keyExtractor={(item) => item.title}
-
+        keyExtractor={item => item.title}
         ListHeaderComponent={<HomeHeader />}
         style={{ flex: 1 }}
-
         showsVerticalScrollIndicator={false}
-
         contentContainerStyle={{
           paddingBottom: insets.bottom + bottomTabBarHeight,
           paddingHorizontal: 16,
-          gap: 16
+          gap: 16,
         }}
         data={data.filter(item => item.enabled)}
       />
