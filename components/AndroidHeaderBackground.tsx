5881afcfec5d8500e1ef268dd25eb817538701b1
aryanjsingh <aryanjsingh07@gmail.com>
2026-04-04 02:19:51 +0530

    fix(android): use native surface color for header

--- components/AndroidHeaderBackground.tsx
+++ components/AndroidHeaderBackground.tsx
@@ -1,11 +1,9 @@
-import { useTheme } from "@react-navigation/native";
 import { Platform, PlatformColor, View } from "react-native";
 
 export default function AndroidHeaderBackground() {
   if(Platform.OS !== "android") return null;
-  const theme = useTheme();
   return (
-    <View style={{ flex: 1, backgroundColor: theme.colors.background, elevation: 4 }} />
+    <View style={{ flex: 1, backgroundColor: PlatformColor("?attr/colorSurface"), elevation: 4 }} />
   )
 }
 
