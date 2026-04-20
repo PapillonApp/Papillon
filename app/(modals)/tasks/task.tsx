bbd2206e4f3b795d7b7da6583526fdf148fb2592
raphckrman <raphckrman@proton.me>
2026-04-20 23:05:08 +0200

    fix: don't display settings if not a custom task

--- app/(modals)/tasks/task.tsx
+++ app/(modals)/tasks/task.tsx
@@ -11,7 +11,6 @@ import { getManager } from "@/services/shared";
 import AnimatedPressable from "@/ui/components/AnimatedPressable";
 import Icon from "@/ui/components/Icon";
 import Stack from "@/ui/components/Stack";
-import TableFlatList from "@/ui/components/TableFlatList";
 import { formatHTML } from "@/utils/format/html";
 import { generateId } from "@/utils/generateId";
 import { getAttachmentIcon } from "@/utils/news/getAttachmentIcon";
@@ -38,16 +37,19 @@ const Task = () => {
     color: getSubjectColor(task.subject),
     emoji: getSubjectEmoji(task.subject),
     name: getSubjectName(task.subject)
-  }
+  };
 
   const [isDone, setIsDone] = useState(task.isDone);
-
   const deleteHomework = useHomeworkActionsStore(s => s.onDelete);
 
   const setAsDone = async (done: boolean) => {
     const manager = getManager();
+
     if (!task.custom) {
-      await manager.setHomeworkCompletion((task as unknown as SharedHomework), done);
+      await manager.setHomeworkCompletion(
+        task as unknown as SharedHomework,
+        done
+      );
     }
 
     const id = generateId(
@@ -59,7 +61,7 @@ const Task = () => {
 
     updateHomeworkIsDone(id, done);
     setIsDone(done);
-  }
+  };
 
   const insets = useSafeAreaInsets();
   const finalHeaderHeight = Platform.select({
@@ -69,22 +71,21 @@ const Task = () => {
 
   return (
     <>
-      {Platform.OS !== 'android' && (
+      {Platform.OS !== "android" && (
         <LinearGradient
           colors={[subjectInfo.color, colors.background]}
           style={{
             position: "absolute",
             top: 0,
             left: 0,
-          right: 0,
-          height: 300,
-          width: "100%",
-          zIndex: -9,
-          opacity: 0.4
-        }}
-      />
+            right: 0,
+            height: 300,
+            width: "100%",
+            zIndex: -9,
+            opacity: 0.4
+          }}
+        />
       )}
-
       <List
         ListHeaderComponent={
           <ModalOverhead
@@ -96,82 +97,18 @@ const Task = () => {
             date={new Date(task.dueDate)}
             style={{
               marginVertical: 24,
-              paddingTop: finalHeaderHeight,
+              paddingTop: finalHeaderHeight
             }}
           />
         }
         style={{
           backgroundColor: "transparent"
         }}
-      />
-      <MenuView
-        onPressAction={async ({ nativeEvent }) => {
-          const actionId = nativeEvent.event;
-          if (actionId === "homework-delete") {
-            deleteHomework!(task)
-            router.back();
-          } else if (actionId === "homework-edit") {
-            router.push({
-              pathname: "/(modals)/tasks/custom",
-              params: {
-                action: "edit",
-                id: generateId(task.subject + task.content + task.createdByAccount + task.dueDate.toDateString()),
-                subject: task.subject,
-                done: task.isDone ? 1 : 0,
-                description: task.content,
-                date: task.dueDate.getTime()
-              }
-            })
-          }
-        }}
-        style={{
-          position: "absolute",
-          right: 20,
-          top: 20
-        }}
-        actions={[
-          {
-            id: "homework-edit",
-            title: "Modifier ce devoir",
-            imageColor: "#000000",
-            image: Platform.select({
-              ios: "paintbrush.fill"
-            }),
-          },
-          {
-            id: "homework-delete",
-            title: "Supprimer ce devoir",
-            imageColor: "#FF0000",
-            image: Platform.select({
-              ios: "trash.fill"
-            }),
-            attributes: { "destructive": true }
-          }
-        ]}>
-      <AnimatedPressable
-        >
-          <Stack
-            card
-            style={{
-              width: 42,
-              height: 42,
-              borderRadius: 30,
-            }}
-            hAlign='center'
-            vAlign='center'
-            noShadow
-            backgroundColor='#FFFFFF50'
-          >
-            <Icon size={26} fill={adjust(subjectInfo.color, theme.dark ? 0.3 : -0.3)}>
-              <Papicons name="Gears" />
-            </Icon>
-          </Stack>
-        </AnimatedPressable>
-        </MenuView>
         contentContainerStyle={{
           padding: 16
         }}
       >
+
         <List.Section>
           <List.SectionTitle>
             <List.Label>{t("Modal_Task_Status")}</List.Label>
@@ -181,7 +118,13 @@ const Task = () => {
             <List.Leading>
               <AnimatedPressable onPress={() => setAsDone(!isDone)}>
                 <Stack
-                  backgroundColor={isDone ? (Platform.OS === 'ios' ? subjectInfo.color : theme.colors.primary) : theme.colors.card}
+                  backgroundColor={
+                    isDone
+                      ? Platform.OS === "ios"
+                        ? subjectInfo.color
+                        : theme.colors.primary
+                      : theme.colors.card
+                  }
                   card
                   radius={100}
                   width={28}
@@ -189,12 +132,13 @@ const Task = () => {
                   vAlign="center"
                   hAlign="center"
                 >
-                  {isDone &&
+                  {isDone && (
                     <Papicons name="check" size={22} color="white" />
-                  }
+                  )}
                 </Stack>
               </AnimatedPressable>
             </List.Leading>
+
             <Typography variant="title">
               {isDone ? t("Task_Done") : t("Task_Undone")}
             </Typography>
@@ -207,9 +151,7 @@ const Task = () => {
           </List.SectionTitle>
 
           <List.Item>
-            <Typography>
-              {formatHTML(task.content)}
-            </Typography>
+            <Typography>{formatHTML(task.content)}</Typography>
           </List.Item>
         </List.Section>
 
@@ -218,90 +160,107 @@ const Task = () => {
             <List.Label>{t("Modal_Task_Attachments")}</List.Label>
           </List.SectionTitle>
 
-          {task.attachments.map((attachment) => (
-            <List.Item onPress={() => WebBrowser.openBrowserAsync(attachment.url, {
-              presentationStyle: "formSheet"
-            })}>
+          {task.attachments.map((attachment, index) => (
+            <List.Item
+              key={index}
+              onPress={() =>
+                WebBrowser.openBrowserAsync(attachment.url, {
+                  presentationStyle: "formSheet"
+                })
+              }
+            >
               <List.Leading>
                 <Icon>
                   <Papicons name={getAttachmentIcon(attachment)} />
                 </Icon>
               </List.Leading>
+
               <Typography variant="title" numberOfLines={1}>
                 {attachment.name || attachment.url}
               </Typography>
-              <Typography variant="body1" color="textSecondary" numberOfLines={1}>
+
+              <Typography
+                variant="body1"
+                color="textSecondary"
+                numberOfLines={1}
+              >
                 {attachment.url}
               </Typography>
             </List.Item>
           ))}
         </List.Section>
       </List>
-      />
+            {task.custom && (
       <MenuView
-        onPressAction={async ({ nativeEvent }) => {
-          const actionId = nativeEvent.event;
-          if (actionId === "homework-delete") {
-            deleteHomework!(task)
-            router.back();
-          } else if (actionId === "homework-edit") {
-            router.push({
-              pathname: "/(modals)/tasks/custom",
-              params: {
-                action: "edit",
-                id: generateId(task.subject + task.content + task.createdByAccount + task.dueDate.toDateString()),
-                subject: task.subject,
-                done: task.isDone ? 1 : 0,
-                description: task.content,
-                date: task.dueDate.getTime()
-              }
-            })
-          }
-        }}
-        style={{
-          position: "absolute",
-          right: 20,
-          top: 20
-        }}
-        actions={[
-          {
-            id: "homework-edit",
-            title: "Modifier ce devoir",
-            imageColor: "#000000",
-            image: Platform.select({
-              ios: "paintbrush.fill"
-            }),
-          },
-          {
-            id: "homework-delete",
-            title: "Supprimer ce devoir",
-            imageColor: "#FF0000",
-            image: Platform.select({
-              ios: "trash.fill"
-            }),
-            attributes: { "destructive": true }
-          }
-        ]}>
-      <AnimatedPressable
+          onPressAction={async ({ nativeEvent }) => {
+            const actionId = nativeEvent.event;
+
+            if (actionId === "homework-delete") {
+              deleteHomework!(task);
+              router.back();
+            } else if (actionId === "homework-edit") {
+              router.push({
+                pathname: "/(modals)/tasks/custom",
+                params: {
+                  action: "edit",
+                  id: generateId(
+                    task.subject +
+                      task.content +
+                      task.createdByAccount +
+                      task.dueDate.toDateString()
+                  ),
+                  subject: task.subject,
+                  done: task.isDone ? 1 : 0,
+                  description: task.content,
+                  date: task.dueDate.getTime()
+                }
+              });
+            }
+          }}
+          style={{
+            position: "absolute",
+            right: 20,
+            top: 20
+          }}
+          actions={[
+            {
+              id: "homework-edit",
+              title: "Modifier ce devoir",
+            },
+            {
+              id: "homework-delete",
+              title: "Supprimer ce devoir",
+              imageColor: "#FF0000",
+              image: Platform.select({
+                ios: "trash.fill"
+              }),
+              attributes: { destructive: true }
+            }
+          ]}
         >
-          <Stack
-            card
-            style={{
-              width: 42,
-              height: 42,
-              borderRadius: 30,
-            }}
-            hAlign='center'
-            vAlign='center'
-            noShadow
-            backgroundColor='#FFFFFF50'
-          >
-            <Icon size={26} fill={adjust(subjectInfo.color, theme.dark ? 0.3 : -0.3)}>
-              <Papicons name="Gears" />
-            </Icon>
-          </Stack>
-        </AnimatedPressable>
+          <AnimatedPressable>
+            <Stack
+              card
+              style={{
+                width: 42,
+                height: 42,
+                borderRadius: 30
+              }}
+              hAlign="center"
+              vAlign="center"
+              noShadow
+              backgroundColor="#FFFFFF50"
+            >
+              <Icon
+                size={26}
+                fill={adjust(subjectInfo.color, theme.dark ? 0.3 : -0.3)}
+              >
+                <Papicons name="Gears" />
+              </Icon>
+            </Stack>
+          </AnimatedPressable>
         </MenuView>
+        )}
     </>
   );
 };
