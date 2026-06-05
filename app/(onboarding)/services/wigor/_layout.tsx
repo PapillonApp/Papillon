c0c4244f98784758d10d10eebdb59c7214fb5812
Kaelian Baudelet <aconique@gmail.com>
2026-06-05 07:49:57 +0200

    chore(onboarding/wigor): add onboarding routes layout

---
+++ app/(onboarding)/services/wigor/_layout.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Stack } from "expo-router";
+import { useScreenOptions } from "@/utils/theme/ScreenOptions";
+import { AndroidHeaderProps } from "@/components/AndroidHeaderBackground";
+
+export default function OnboardingLayout() {
+  const screenOptions = useScreenOptions();
+  const newScreenOptions = React.useMemo(
+    () => ({
+      ...screenOptions,
+      headerShown: false,
+      ...AndroidHeaderProps,
+      headerTransparent: true,
+      headerBackButtonDisplayMode: "minimal",
+      headerLargeTitle: false,
+    }),
+    [screenOptions]
+  );
+
+  return (
+    <Stack>
+      <Stack.Screen name="list" options={{ ...newScreenOptions }} />
+      <Stack.Screen name="credentials" options={{ ...newScreenOptions }} />
+    </Stack>
+  );
+}
