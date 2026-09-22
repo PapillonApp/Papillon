bc2d463fa4e25a2c81df661e22ac368955953813
Nathanaël BUBBE <nathabu92@gmail.com>
2026-09-22 20:45:23 +0200

    update typography
    error : Uncaught ReferenceError: variantStyles is not defined
        at index-c926ac578cf73e3946e89d83d7a8a9da.js:1211:2853
        at co (index-c926ac578cf73e3946e89d83d7a8a9da.js:166:53709)
        at ri (index-c926ac578cf73e3946e89d83d7a8a9da.js:166:76687)
        at ki (index-c926ac578cf73e3946e89d83d7a8a9da.js:166:86948)
        at Pc (index-c926ac578cf73e3946e89d83d7a8a9da.js:166:122027)
        at xc (index-c926ac578cf73e3946e89d83d7a8a9da.js:166:121079)
        at Ec (index-c926ac578cf73e3946e89d83d7a8a9da.js:166:120921)
        at fc (index-c926ac578cf73e3946e89d83d7a8a9da.js:166:117748)
        at rf (index-c926ac578cf73e3946e89d83d7a8a9da.js:166:129395)
        at MessagePort.q (index-c926ac578cf73e3946e89d83d7a8a9da.js:178:1531)

--- ui/components/Typography.tsx
+++ ui/components/Typography.tsx
@@ -231,6 +231,7 @@ const Typography = React.memo(({
               color;
 
   const secondaryOpacity = color === "secondary" ? { opacity: 0.5 } : undefined;
+  const variantStyles = VARIANTS[variant] ?? VARIANTS.body1;
   const variantFont = (variantStyles.fontFamily as string) || FONT_FAMILIES.medium;
   const resolvedFont = weight ? font(weight) : font(variantFont);
 
